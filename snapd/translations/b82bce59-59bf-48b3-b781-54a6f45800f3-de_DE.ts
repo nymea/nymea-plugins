70af072694f463334274603dddbc2da7e1999c26
Jenkins <jenkins@nymea.io>
2018-01-31 17:36:19 +0100

    Automatic translations update by Jenkins

--- snapd/translations/b82bce59-59bf-48b3-b781-54a6f45800f3-de_DE.ts
+++ snapd/translations/b82bce59-59bf-48b3-b781-54a6f45800f3-de_DE.ts
@@ -4,193 +4,193 @@
 <context>
     <name>Snapd</name>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="65"/>
+        <location filename="../plugininfo.h" line="65"/>
         <source>Snapd</source>
         <extracomment>The name of the plugin Snapd (b82bce59-59bf-48b3-b781-54a6f45800f3)</extracomment>
         <translation>Snapd</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="68"/>
+        <location filename="../plugininfo.h" line="68"/>
         <source>Advanced mode</source>
         <extracomment>The name of the paramType (017fe4c5-fc41-41fe-8e67-08fdaccb89ea) of Snapd</extracomment>
         <translation>Fortgeschrittener Modus</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="71"/>
+        <location filename="../plugininfo.h" line="71"/>
         <source>Canonical</source>
         <extracomment>The name of the vendor (60582ddf-32ea-4fcd-a6f2-f3beaaf21517)</extracomment>
         <translation>Canonical</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="83"/>
+        <location filename="../plugininfo.h" line="83"/>
         <source>System update available changed</source>
         <extracomment>The name of the autocreated EventType (a6b1d24b-d523-4516-9bce-5b467e5e09b2)</extracomment>
         <translation>Systemupdate Verfügbarkeit geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="86"/>
+        <location filename="../plugininfo.h" line="86"/>
         <source>System update available</source>
         <extracomment>The name of the ParamType of StateType (a6b1d24b-d523-4516-9bce-5b467e5e09b2) of DeviceClass Update manager</extracomment>
         <translation>Systemupdate verfügbar</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="95"/>
+        <location filename="../plugininfo.h" line="95"/>
         <source>Last automatic system update time changed</source>
         <extracomment>The name of the autocreated EventType (c671545a-6bde-4c08-8e37-0d256841a3a5)</extracomment>
         <translation>Letztes automatisches Systemupdate geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="98"/>
+        <location filename="../plugininfo.h" line="98"/>
         <source>Last automatic system update</source>
         <extracomment>The name of the ParamType of StateType (c671545a-6bde-4c08-8e37-0d256841a3a5) of DeviceClass Update manager</extracomment>
         <translation>Letztes automatisches Systemupdate</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="101"/>
+        <location filename="../plugininfo.h" line="101"/>
         <source>Next automatic system update time changed</source>
         <extracomment>The name of the autocreated EventType (122c2423-a1d9-400f-80f8-b1f798975914)</extracomment>
         <translation>Nächstes automatisches Systemupdate geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="104"/>
+        <location filename="../plugininfo.h" line="104"/>
         <source>Next automatic system update</source>
         <extracomment>The name of the ParamType of StateType (122c2423-a1d9-400f-80f8-b1f798975914) of DeviceClass Update manager</extracomment>
         <translation>Nächstes automatisches Systemupdate</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="125"/>
+        <location filename="../plugininfo.h" line="125"/>
         <source>ID</source>
         <extracomment>The name of the paramType (9afb98fb-f717-4f4c-8009-1a6514054c5f) of Snap</extracomment>
         <translation>ID</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="128"/>
+        <location filename="../plugininfo.h" line="128"/>
         <source>Summary</source>
         <extracomment>The name of the paramType (12b9a65f-970b-49b5-b1d0-1625fc6d8758) of Snap</extracomment>
         <translation>Zusammenfassung</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="131"/>
+        <location filename="../plugininfo.h" line="131"/>
         <source>Description</source>
         <extracomment>The name of the paramType (fe24c61b-e154-4259-b7ca-6f0602e9d1c3) of Snap</extracomment>
         <translation>Beschreibung</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="134"/>
+        <location filename="../plugininfo.h" line="134"/>
         <source>Developer</source>
         <extracomment>The name of the paramType (76ead9c5-0a18-40a2-b31d-f6bb6dfea0a5) of Snap</extracomment>
         <translation>Entwickler</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="140"/>
+        <location filename="../plugininfo.h" line="140"/>
         <source>Channel</source>
         <extracomment>The name of the ParamType of StateType (7be2b61e-3f59-4b92-b2bb-50d027bb92ff) of DeviceClass Snap</extracomment>
         <translation>Kanal</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="143"/>
+        <location filename="../plugininfo.h" line="143"/>
         <source>Set channel</source>
         <extracomment>The name of the autocreated ActionType (7be2b61e-3f59-4b92-b2bb-50d027bb92ff)</extracomment>
         <translation>Setze Kanal</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="146"/>
+        <location filename="../plugininfo.h" line="146"/>
         <source>Version changed</source>
         <extracomment>The name of the autocreated EventType (532a95f3-db29-427e-bb32-d5a22029e586)</extracomment>
         <translation>Version geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="149"/>
+        <location filename="../plugininfo.h" line="149"/>
         <source>Version</source>
         <extracomment>The name of the ParamType of StateType (532a95f3-db29-427e-bb32-d5a22029e586) of DeviceClass Snap</extracomment>
         <translation>Version</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="152"/>
+        <location filename="../plugininfo.h" line="152"/>
         <source>Revision changed</source>
         <extracomment>The name of the autocreated EventType (f26a6404-e011-11e7-9224-2350048461eb)</extracomment>
         <translation>Revision geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="155"/>
+        <location filename="../plugininfo.h" line="155"/>
         <source>Revision</source>
         <extracomment>The name of the ParamType of StateType (f26a6404-e011-11e7-9224-2350048461eb) of DeviceClass Snap</extracomment>
         <translation>Revision</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="158"/>
+        <location filename="../plugininfo.h" line="158"/>
         <source>Rollback to previous version</source>
         <extracomment>The name of the ActionType e061dee6-62fc-45cc-9c9f-403c2be52939 of deviceClass Snap</extracomment>
         <translation>Zurücksetzen auf vorhergehende Version</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="113"/>
+        <location filename="../plugininfo.h" line="113"/>
         <source>Start update</source>
         <extracomment>The name of the ActionType 45626b75-f09d-4dd1-b6c4-ee33201b47b0 of deviceClass Update manager</extracomment>
         <translation>Starte Systemupdate</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="74"/>
+        <location filename="../plugininfo.h" line="74"/>
         <source>Update manager</source>
         <extracomment>The name of the DeviceClass (d90cda58-4d8c-4b7f-a982-38e56a95b72a)</extracomment>
         <translation>Update Manager</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="77"/>
+        <location filename="../plugininfo.h" line="77"/>
         <source>Update manager available changed</source>
         <extracomment>The name of the autocreated EventType (6b662b3e-fd12-4f24-be77-aec066f16d8c)</extracomment>
         <translation>Update Manager Verfügbarkeit geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="80"/>
+        <location filename="../plugininfo.h" line="80"/>
         <source>Update manager available</source>
         <extracomment>The name of the ParamType of StateType (6b662b3e-fd12-4f24-be77-aec066f16d8c) of DeviceClass Update manager</extracomment>
         <translation>Update Manager verfügbar</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="119"/>
+        <location filename="../plugininfo.h" line="119"/>
         <source>Snap</source>
         <extracomment>The name of the DeviceClass (ff0840d7-fcfc-4403-9d9f-301610d5a437)</extracomment>
         <translation>Snap</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="89"/>
+        <location filename="../plugininfo.h" line="89"/>
         <source>System update running changed</source>
         <extracomment>The name of the autocreated EventType (01ca7a22-5607-4c5e-a465-a2ae7e8b529c)</extracomment>
         <translation>Systemupdate aktiv geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="92"/>
+        <location filename="../plugininfo.h" line="92"/>
         <source>System update running</source>
         <extracomment>The name of the ParamType of StateType (01ca7a22-5607-4c5e-a465-a2ae7e8b529c) of DeviceClass Update manager</extracomment>
         <translation>Systemupdate aktiv</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="107"/>
+        <location filename="../plugininfo.h" line="107"/>
         <source>Status changed</source>
         <extracomment>The name of the autocreated EventType (4987aca3-3916-4cb3-938f-df6c99d04dbf)</extracomment>
         <translation>Status geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="110"/>
+        <location filename="../plugininfo.h" line="110"/>
         <source>Status</source>
         <extracomment>The name of the ParamType of StateType (4987aca3-3916-4cb3-938f-df6c99d04dbf) of DeviceClass Update manager</extracomment>
         <translation>Status</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="122"/>
+        <location filename="../plugininfo.h" line="122"/>
         <source>Name</source>
         <extracomment>The name of the paramType (4f38614d-8be0-48dc-a24d-cee9ff1f2a89) of Snap</extracomment>
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="137"/>
+        <location filename="../plugininfo.h" line="137"/>
         <source>Channel changed</source>
         <extracomment>The name of the autocreated EventType (7be2b61e-3f59-4b92-b2bb-50d027bb92ff)</extracomment>
         <translation>Kanal geändert</translation>
     </message>
     <message>
-        <location filename="../../../build-guh-plugins-Desktop-Debug/snapd/plugininfo.h" line="116"/>
+        <location filename="../plugininfo.h" line="116"/>
         <source>Check for updates</source>
         <extracomment>The name of the ActionType 4738f2c9-666e-45b9-91d3-7bcbf722b669 of deviceClass Update manager</extracomment>
         <translation>Nach Aktualisierungen suchen</translation>
