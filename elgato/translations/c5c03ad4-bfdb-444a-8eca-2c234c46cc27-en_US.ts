aa507c1691dfc005a7bb4231ec5f927c8f5d4beb
Jenkins <jenkins@nymea.io>
2018-02-09 15:26:03 +0100

    Automatic translations update by Jenkins

--- elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-en_US.ts
+++ elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-en_US.ts
@@ -4,8 +4,8 @@
 <context>
     <name>Elgato</name>
     <message>
-        <location filename="../plugininfo.h" line="37"/>
         <location filename="../plugininfo.h" line="40"/>
+        <location filename="../plugininfo.h" line="43"/>
         <source>Elgato</source>
         <extracomment>The name of the plugin Elgato (c5c03ad4-bfdb-444a-8eca-2c234c46cc27)
 ----------
@@ -13,49 +13,67 @@ The name of the vendor (90a3091d-1053-4f77-8dc3-92e27bbcebe7)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="43"/>
+        <location filename="../plugininfo.h" line="46"/>
         <source>Avea</source>
         <extracomment>The name of the DeviceClass (164f9602-90ee-4693-bda3-9cafae37603e)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="46"/>
+        <location filename="../plugininfo.h" line="49"/>
         <source>name</source>
         <extracomment>The name of the paramType (d7a5d96f-33dc-4dc3-a171-b1e89a5e1a4b) of avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="49"/>
+        <location filename="../plugininfo.h" line="52"/>
         <source>mac address</source>
         <extracomment>The name of the paramType (20932e39-00fd-40e3-8f39-850ba149844e) of avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="61"/>
+        <location filename="../plugininfo.h" line="55"/>
+        <source>connected changed</source>
+        <extracomment>The name of the autocreated EventType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="58"/>
+        <source>connected</source>
+        <extracomment>The name of the ParamType of StateType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="70"/>
         <source>color changed</source>
         <extracomment>The name of the autocreated EventType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="52"/>
+        <location filename="../plugininfo.h" line="76"/>
+        <source>Set color</source>
+        <extracomment>The name of the autocreated ActionType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="61"/>
         <source>brightness changed</source>
         <extracomment>The name of the autocreated EventType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="55"/>
+        <location filename="../plugininfo.h" line="64"/>
         <source>brightness</source>
         <extracomment>The name of the ParamType of StateType (c38181a0-e19b-423f-8b66-dedda94e89b5) of DeviceClass avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="58"/>
+        <location filename="../plugininfo.h" line="67"/>
         <source>Set brightness</source>
         <extracomment>The name of the autocreated ActionType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="64"/>
+        <location filename="../plugininfo.h" line="73"/>
         <source>color</source>
         <extracomment>The name of the ParamType of StateType (3f15637a-8272-4714-bd08-04806e05bbef) of DeviceClass avea</extracomment>
         <translation type="unfinished"></translation>
--- senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-en_US.ts
+++ senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-en_US.ts
@@ -4,8 +4,8 @@
 <context>
     <name>Senic</name>
     <message>
-        <location filename="../plugininfo.h" line="52"/>
-        <location filename="../plugininfo.h" line="55"/>
+        <location filename="../plugininfo.h" line="54"/>
+        <location filename="../plugininfo.h" line="57"/>
         <source>Senic</source>
         <extracomment>The name of the plugin Senic (413e9d77-335f-4ecf-abbc-8f2a8a399c39)
 ----------
@@ -13,121 +13,133 @@ The name of the vendor (8e6cae9c-706b-49e5-ae86-da52a20dd336)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="58"/>
+        <location filename="../plugininfo.h" line="60"/>
         <source>Nuimo</source>
         <extracomment>The name of the DeviceClass (315ece51-053e-49f9-831f-b09f9f27fb9f)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="61"/>
+        <location filename="../plugininfo.h" line="63"/>
         <source>name</source>
         <extracomment>The name of the paramType (db67d1e6-26fa-44ed-ad55-c6aef45ea2ea) of nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="64"/>
+        <location filename="../plugininfo.h" line="66"/>
         <source>mac address</source>
         <extracomment>The name of the paramType (71553f6a-2ed4-4896-bb7b-52e7dca948b2) of nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="70"/>
+        <location filename="../plugininfo.h" line="72"/>
         <source>connected</source>
         <extracomment>The name of the ParamType of StateType (5286976a-f5dc-4662-872a-438ac5d491cb) of DeviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="76"/>
+        <location filename="../plugininfo.h" line="75"/>
+        <source>Hardware revision changed</source>
+        <extracomment>The name of the autocreated EventType (5c400da4-a14e-4e0a-be9f-c82ffe7e1972)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="78"/>
+        <source>hardwareRevision</source>
+        <extracomment>The name of the ParamType of StateType (5c400da4-a14e-4e0a-be9f-c82ffe7e1972) of DeviceClass nuimo</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="84"/>
         <source>firmwareRevision</source>
         <extracomment>The name of the ParamType of StateType (edcf76c6-9fed-4c26-9853-c284cf887adb) of DeviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="82"/>
+        <location filename="../plugininfo.h" line="90"/>
         <source>softwareRevision</source>
         <extracomment>The name of the ParamType of StateType (be42cbd3-12e9-44ec-8f9d-141e10d9573a) of DeviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="85"/>
+        <location filename="../plugininfo.h" line="93"/>
         <source>battery changed</source>
         <extracomment>The name of the autocreated EventType (b5ee2465-7fa1-450b-8073-f115537d3409)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="88"/>
+        <location filename="../plugininfo.h" line="96"/>
         <source>battery</source>
         <extracomment>The name of the ParamType of StateType (b5ee2465-7fa1-450b-8073-f115537d3409) of DeviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="91"/>
+        <location filename="../plugininfo.h" line="99"/>
         <source>rotation changed</source>
         <extracomment>The name of the autocreated EventType (69a5f495-5452-434b-8fb8-b73d992c5446)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="94"/>
+        <location filename="../plugininfo.h" line="102"/>
         <source>rotation</source>
         <extracomment>The name of the ParamType of StateType (69a5f495-5452-434b-8fb8-b73d992c5446) of DeviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="67"/>
+        <location filename="../plugininfo.h" line="69"/>
         <source>Connected changed</source>
         <extracomment>The name of the autocreated EventType (5286976a-f5dc-4662-872a-438ac5d491cb)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="73"/>
+        <location filename="../plugininfo.h" line="81"/>
         <source>Firmware revision changed</source>
         <extracomment>The name of the autocreated EventType (edcf76c6-9fed-4c26-9853-c284cf887adb)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="79"/>
+        <location filename="../plugininfo.h" line="87"/>
         <source>Software revision changed</source>
         <extracomment>The name of the autocreated EventType (be42cbd3-12e9-44ec-8f9d-141e10d9573a)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="97"/>
+        <location filename="../plugininfo.h" line="105"/>
         <source>Show logo</source>
         <extracomment>The name of the ActionType d44ca5b7-f8d6-4413-9d2e-cef89282c039 of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="100"/>
+        <location filename="../plugininfo.h" line="108"/>
         <source>logo</source>
         <extracomment>The name of the paramType (a8f72c37-9cb5-4885-a7e4-b2b396f8e4cd) of nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="103"/>
+        <location filename="../plugininfo.h" line="111"/>
         <source>clicked</source>
         <extracomment>The name of the EventType 5b9e08e8-7a6c-4311-9db6-82547847708c of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="106"/>
+        <location filename="../plugininfo.h" line="114"/>
         <source>Swipe left</source>
         <extracomment>The name of the EventType 2be36aa0-e2fe-4192-81c5-cf0bb7f08dd4 of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="109"/>
+        <location filename="../plugininfo.h" line="117"/>
         <source>Swipe right</source>
         <extracomment>The name of the EventType 81fb61ab-6d3d-4c1b-85fe-3dbd5c8dead7 of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="112"/>
+        <location filename="../plugininfo.h" line="120"/>
         <source>Swipe up</source>
         <extracomment>The name of the EventType ab5c575b-b265-491c-93ca-ad9212374bc1 of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="115"/>
+        <location filename="../plugininfo.h" line="123"/>
         <source>Swipe down</source>
         <extracomment>The name of the EventType 4d97cf28-1030-4a30-bed5-411102dd4b9b of deviceClass nuimo</extracomment>
         <translation type="unfinished"></translation>
