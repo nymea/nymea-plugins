42d593c06e25ac1c990a54056a51524c79428844
Simon Stürz <simon.stuerz@guh.io>
2018-03-12 18:33:42 +0100

    Add debian package of keba plugin and add translations

---
+++ keba/translations/9142b09f-30a9-43d0-9ede-2f8debe075ac-en_US.ts
@@ -0,0 +1,127 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>KebaKeContact</name>
+    <message>
+        <location filename="../plugininfo.h" line="51"/>
+        <source>KebaKeContact</source>
+        <extracomment>The name of the plugin KebaKeContact (9142b09f-30a9-43d0-9ede-2f8debe075ac)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="54"/>
+        <source>Keba</source>
+        <extracomment>The name of the vendor (f7cda40b-829a-4675-abaa-485697430f5f)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="57"/>
+        <source>Keba KeContact P30</source>
+        <extracomment>The name of the DeviceClass (900dacec-cae7-4a37-95ba-501846368ea2)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="60"/>
+        <source>IP Address</source>
+        <extracomment>The name of the paramType (730cd3d3-5f0e-4028-a8c2-ced7574f13f3) of wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="63"/>
+        <source>Activity changed</source>
+        <extracomment>The name of the autocreated EventType (539e5602-6dd9-465d-9705-3bb59bcf8982)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="66"/>
+        <source>activity</source>
+        <extracomment>The name of the ParamType of StateType (539e5602-6dd9-465d-9705-3bb59bcf8982) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="69"/>
+        <source>Plug State changed</source>
+        <extracomment>The name of the autocreated EventType (3b4d29f3-3101-47ad-90fd-269b6348783b)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="72"/>
+        <source>plugState</source>
+        <extracomment>The name of the ParamType of StateType (3b4d29f3-3101-47ad-90fd-269b6348783b) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="75"/>
+        <source>Current changed</source>
+        <extracomment>The name of the autocreated EventType (a29c1748-fe97-4830-a56e-e1cc4e618385)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="78"/>
+        <source>current</source>
+        <extracomment>The name of the ParamType of StateType (a29c1748-fe97-4830-a56e-e1cc4e618385) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="81"/>
+        <source>Maximal Current changed</source>
+        <extracomment>The name of the autocreated EventType (593656f0-babf-4308-8767-68f34e10fb15)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="84"/>
+        <source>maxCurrent</source>
+        <extracomment>The name of the ParamType of StateType (593656f0-babf-4308-8767-68f34e10fb15) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="87"/>
+        <source>Set maximal current</source>
+        <extracomment>The name of the autocreated ActionType (593656f0-babf-4308-8767-68f34e10fb15)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="90"/>
+        <source>Power changed</source>
+        <extracomment>The name of the autocreated EventType (e8f069ca-7fa7-4568-8d4c-165f6d048720)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="93"/>
+        <source>power</source>
+        <extracomment>The name of the ParamType of StateType (e8f069ca-7fa7-4568-8d4c-165f6d048720) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="96"/>
+        <source>Output Enable changed</source>
+        <extracomment>The name of the autocreated EventType (0cd5396a-bc41-4c8f-b037-db10991a76c7)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="99"/>
+        <source>outEnable</source>
+        <extracomment>The name of the ParamType of StateType (0cd5396a-bc41-4c8f-b037-db10991a76c7) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="102"/>
+        <source>Set Output</source>
+        <extracomment>The name of the autocreated ActionType (0cd5396a-bc41-4c8f-b037-db10991a76c7)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="105"/>
+        <source>Device Reachable changed</source>
+        <extracomment>The name of the autocreated EventType (b1a574a6-46b6-44ea-a0bb-9b4de3198967)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="108"/>
+        <source>reachable</source>
+        <extracomment>The name of the ParamType of StateType (b1a574a6-46b6-44ea-a0bb-9b4de3198967) of DeviceClass wallbox</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
