1dfdca8c89dbb1f2b696c287e49188a086d4470b
Jenkins <jenkins@nymea.io>
2018-03-15 11:52:10 +0100

    Automatic translations update by Jenkins

--- httpcommander/translations/4e62670c-6268-4487-8dff-cccca498731a-en_US.ts
+++ httpcommander/translations/4e62670c-6268-4487-8dff-cccca498731a-en_US.ts
@@ -1,4 +1,107 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+<context>
+    <name>HttpCommander</name>
+    <message>
+        <source>HttpCommander</source>
+        <extracomment>The name of the plugin HttpCommander (4e62670c-6268-4487-8dff-cccca498731a)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HTTP commander</source>
+        <extracomment>The name of the vendor (45d7c941-7690-43c9-92fc-fab36e1cebd0)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HTTP post commander</source>
+        <extracomment>The name of the DeviceClass (b101abdf-86fd-4d2e-a657-ee76044235bd)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Address</source>
+        <extracomment>The name of the paramType (020f672e-cc9a-4b74-92dd-a92a93ab1d23) of httpPostCommander
+----------
+The name of the paramType (1a3fcb23-931b-4ba1-b134-c49b656c76f7) of httpPutCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <extracomment>The name of the paramType (37830ea8-2249-46e6-aaca-12164928a81a) of httpPostCommander
+----------
+The name of the paramType (db994349-1105-4ce5-b6fe-6fd38fbc436a) of httpPutCommander
+----------
+The name of the paramType (bee8b151-815a-4159-9d8a-42b76e99b42c) of httpGetCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reachability changed</source>
+        <extracomment>The name of the autocreated EventType (8daac0e7-4c2f-4cdf-b528-02cfe04c6b39)
+----------
+The name of the autocreated EventType (d102ff86-b773-48e3-a7a5-e138cb541f49)
+----------
+The name of the autocreated EventType (0d63f815-efd1-488a-9bfa-e9f6bda540d2)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>connected</source>
+        <extracomment>The name of the ParamType of StateType (8daac0e7-4c2f-4cdf-b528-02cfe04c6b39) of DeviceClass httpPostCommander
+----------
+The name of the ParamType of StateType (d102ff86-b773-48e3-a7a5-e138cb541f49) of DeviceClass httpPutCommander
+----------
+The name of the ParamType of StateType (0d63f815-efd1-488a-9bfa-e9f6bda540d2) of DeviceClass httpGetCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Response received</source>
+        <extracomment>The name of the autocreated EventType (69f32ec8-114d-43f4-9241-1f6a57261f32)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>response</source>
+        <extracomment>The name of the ParamType of StateType (69f32ec8-114d-43f4-9241-1f6a57261f32) of DeviceClass httpPostCommander
+----------
+The name of the ParamType of StateType (69f32ec8-114d-43f4-9241-1f6a57261f32) of DeviceClass httpPutCommander
+----------
+The name of the ParamType of StateType (d81f0644-b94e-48ed-ae48-1b8ff6cebc0c) of DeviceClass httpGetCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Post data</source>
+        <extracomment>The name of the ActionType 5a97ca56-b334-411b-adba-116496ffe83d of deviceClass httpPostCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <extracomment>The name of the paramType (363119a3-c02c-4ed5-a915-11706198f3eb) of httpPostCommander
+----------
+The name of the paramType (7742d445-8fc1-4b20-87f2-1bb35929fce1) of httpPutCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HTTP put commander</source>
+        <extracomment>The name of the DeviceClass (05bf65f5-ff13-43e3-b6ae-77019e79d8a1)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>put</source>
+        <extracomment>The name of the ActionType a9f165dc-cdf1-48f0-b4b6-7c24373cb77c of deviceClass httpPutCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>HTTP get</source>
+        <extracomment>The name of the DeviceClass (8f3f6dde-9db3-4237-800b-bb7f804098c9)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>URL or IPv4 Address</source>
+        <extracomment>The name of the paramType (477b544b-b631-4526-a4ef-c712ff5f955d) of httpGetCommander</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Response data received</source>
+        <extracomment>The name of the autocreated EventType (d81f0644-b94e-48ed-ae48-1b8ff6cebc0c)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
