22ad95d747a5e13c963c37a513ade5da0d1e657b
Jenkins <jenkins@nymea.io>
2018-03-19 10:30:55 +0100

    Automatic translations update by Jenkins

--- elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-de_DE.ts
+++ elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-de_DE.ts
@@ -4,8 +4,8 @@
 <context>
     <name>Elgato</name>
     <message>
-        <location filename="../plugininfo.h" line="40"/>
-        <location filename="../plugininfo.h" line="43"/>
+        <location filename="../plugininfo.h" line="64"/>
+        <location filename="../plugininfo.h" line="67"/>
         <source>Elgato</source>
         <extracomment>The name of the plugin Elgato (c5c03ad4-bfdb-444a-8eca-2c234c46cc27)
 ----------
@@ -13,69 +13,177 @@ The name of the vendor (90a3091d-1053-4f77-8dc3-92e27bbcebe7)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="46"/>
+        <location filename="../plugininfo.h" line="70"/>
         <source>Avea</source>
         <extracomment>The name of the DeviceClass (164f9602-90ee-4693-bda3-9cafae37603e)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="49"/>
-        <source>name</source>
-        <extracomment>The name of the paramType (d7a5d96f-33dc-4dc3-a171-b1e89a5e1a4b) of avea</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="52"/>
-        <source>mac address</source>
-        <extracomment>The name of the paramType (20932e39-00fd-40e3-8f39-850ba149844e) of avea</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="55"/>
-        <source>connected changed</source>
-        <extracomment>The name of the autocreated EventType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc)</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="58"/>
+        <location filename="../plugininfo.h" line="82"/>
         <source>connected</source>
         <extracomment>The name of the ParamType of StateType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc) of DeviceClass avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="70"/>
-        <source>color changed</source>
-        <extracomment>The name of the autocreated EventType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="76"/>
+        <location filename="../plugininfo.h" line="109"/>
         <source>Set color</source>
         <extracomment>The name of the autocreated ActionType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="61"/>
-        <source>brightness changed</source>
+        <location filename="../plugininfo.h" line="73"/>
+        <source>Name</source>
+        <extracomment>The name of the paramType (d7a5d96f-33dc-4dc3-a171-b1e89a5e1a4b) of avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="76"/>
+        <source>MAC address</source>
+        <extracomment>The name of the paramType (20932e39-00fd-40e3-8f39-850ba149844e) of avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="79"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the autocreated EventType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="85"/>
+        <source>Power changed</source>
+        <extracomment>The name of the autocreated EventType (2b779968-ff4f-4a05-a466-074e14288a8c)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="88"/>
+        <source>power</source>
+        <extracomment>The name of the ParamType of StateType (2b779968-ff4f-4a05-a466-074e14288a8c) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="91"/>
+        <source>Set power</source>
+        <extracomment>The name of the autocreated ActionType (2b779968-ff4f-4a05-a466-074e14288a8c)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="94"/>
+        <source>Brightness changed</source>
         <extracomment>The name of the autocreated EventType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="64"/>
+        <location filename="../plugininfo.h" line="97"/>
         <source>brightness</source>
         <extracomment>The name of the ParamType of StateType (c38181a0-e19b-423f-8b66-dedda94e89b5) of DeviceClass avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="67"/>
+        <location filename="../plugininfo.h" line="100"/>
         <source>Set brightness</source>
         <extracomment>The name of the autocreated ActionType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="73"/>
+        <location filename="../plugininfo.h" line="103"/>
+        <source>Color changed</source>
+        <extracomment>The name of the autocreated EventType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="106"/>
         <source>color</source>
         <extracomment>The name of the ParamType of StateType (3f15637a-8272-4714-bd08-04806e05bbef) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="112"/>
+        <source>white changed</source>
+        <extracomment>The name of the autocreated EventType (50f6a224-fb8d-487d-8774-1a0536e5b1aa)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="115"/>
+        <source>white</source>
+        <extracomment>The name of the ParamType of StateType (50f6a224-fb8d-487d-8774-1a0536e5b1aa) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="118"/>
+        <source>Set white</source>
+        <extracomment>The name of the autocreated ActionType (50f6a224-fb8d-487d-8774-1a0536e5b1aa)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="121"/>
+        <source>Red changed</source>
+        <extracomment>The name of the autocreated EventType (50ab71d1-8970-4531-b1ad-f387d01bab90)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="124"/>
+        <source>red</source>
+        <extracomment>The name of the ParamType of StateType (50ab71d1-8970-4531-b1ad-f387d01bab90) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="127"/>
+        <source>Set red</source>
+        <extracomment>The name of the autocreated ActionType (50ab71d1-8970-4531-b1ad-f387d01bab90)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="130"/>
+        <source>Green changed</source>
+        <extracomment>The name of the autocreated EventType (9e8ddebc-33fe-418a-895f-d8148fabcfee)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="133"/>
+        <source>green</source>
+        <extracomment>The name of the ParamType of StateType (9e8ddebc-33fe-418a-895f-d8148fabcfee) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="136"/>
+        <source>Set green</source>
+        <extracomment>The name of the autocreated ActionType (9e8ddebc-33fe-418a-895f-d8148fabcfee)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="139"/>
+        <source>Blue changed</source>
+        <extracomment>The name of the autocreated EventType (f4310747-d468-4d2e-abe4-b5e4d0245dad)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="142"/>
+        <source>blue</source>
+        <extracomment>The name of the ParamType of StateType (f4310747-d468-4d2e-abe4-b5e4d0245dad) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="145"/>
+        <source>Set blue</source>
+        <extracomment>The name of the autocreated ActionType (f4310747-d468-4d2e-abe4-b5e4d0245dad)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="148"/>
+        <source>Fade changed</source>
+        <extracomment>The name of the autocreated EventType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="151"/>
+        <source>fade</source>
+        <extracomment>The name of the ParamType of StateType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="154"/>
+        <source>Set fade</source>
+        <extracomment>The name of the autocreated ActionType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d)</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
--- gpio/translations/127ead55-996a-44ac-ba82-fc3c634e018a-de_DE.ts
+++ gpio/translations/127ead55-996a-44ac-ba82-fc3c634e018a-de_DE.ts
@@ -11,14 +11,14 @@
     </message>
     <message>
         <location filename="../plugininfo.h" line="60"/>
-        <source>Raspberry Pi 2</source>
+        <source>Raspberry Pi 2/3</source>
         <extracomment>The name of the vendor (f0d00b66-bbd8-4a07-8591-ea48a61b229e)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="63"/>
         <location filename="../plugininfo.h" line="105"/>
-        <source>GPIO Switch</source>
+        <source>GPIO Output</source>
         <extracomment>The name of the DeviceClass (3885c520-e202-4435-88f6-3c35c362b2e6)
 ----------
 The name of the DeviceClass (75d54a59-f9b0-4bc4-a86c-6b1fc47e0663)</extracomment>
@@ -30,13 +30,13 @@ The name of the DeviceClass (75d54a59-f9b0-4bc4-a86c-6b1fc47e0663)</extracomment
         <location filename="../plugininfo.h" line="108"/>
         <location filename="../plugininfo.h" line="129"/>
         <source>GPIO</source>
-        <extracomment>The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioSwitchRpi
+        <extracomment>The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioOutputRpi
 ----------
-The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioButtonRpi
+The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioInputRpi
 ----------
-The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioSwitchBbb
+The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioOutputBbb
 ----------
-The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioButtonBbb</extracomment>
+The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioInputBbb</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -45,13 +45,13 @@ The name of the paramType (9eda783f-6d9f-4d39-986d-d2cbfff5a7dd) of gpioButtonBb
         <location filename="../plugininfo.h" line="111"/>
         <location filename="../plugininfo.h" line="132"/>
         <source>Pin number</source>
-        <extracomment>The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioSwitchRpi
+        <extracomment>The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioOutputRpi
 ----------
-The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioButtonRpi
+The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioInputRpi
 ----------
-The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioSwitchBbb
+The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioOutputBbb
 ----------
-The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioButtonBbb</extracomment>
+The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioInputBbb</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -60,13 +60,13 @@ The name of the paramType (2204d278-7bc7-407f-ac82-ce3ae1d5779c) of gpioButtonBb
         <location filename="../plugininfo.h" line="114"/>
         <location filename="../plugininfo.h" line="135"/>
         <source>Description</source>
-        <extracomment>The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioSwitchRpi
+        <extracomment>The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioOutputRpi
 ----------
-The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioButtonRpi
+The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioInputRpi
 ----------
-The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioSwitchBbb
+The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioOutputBbb
 ----------
-The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioButtonBbb</extracomment>
+The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioInputBbb</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -80,18 +80,27 @@ The name of the paramType (504798eb-1faa-4703-a57a-2778e4bf9a67) of gpioButtonBb
         <location filename="../plugininfo.h" line="78"/>
         <location filename="../plugininfo.h" line="120"/>
         <source>powerValue</source>
-        <extracomment>The name of the ParamType of StateType (06843766-358e-44b0-8d52-2b46ef98459a) of DeviceClass gpioSwitchRpi
+        <extracomment>The name of the ParamType of StateType (06843766-358e-44b0-8d52-2b46ef98459a) of DeviceClass gpioOutputRpi
 ----------
-The name of the ParamType of StateType (06843766-358e-44b0-8d52-2b46ef98459a) of DeviceClass gpioSwitchBbb</extracomment>
+The name of the ParamType of StateType (06843766-358e-44b0-8d52-2b46ef98459a) of DeviceClass gpioOutputBbb</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="84"/>
+        <location filename="../plugininfo.h" line="126"/>
+        <source>GPIO Input</source>
+        <extracomment>The name of the DeviceClass (6aff228b-0410-4ef9-9593-51e8639aacea)
+----------
+The name of the DeviceClass (ffd2aa29-55cc-4824-ba95-c311784f7824)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="99"/>
         <location filename="../plugininfo.h" line="141"/>
         <source>pressed</source>
-        <extracomment>The name of the ParamType of StateType (57f1b7cc-26c8-434b-ba04-d3077dc886c8) of DeviceClass gpioButtonRpi
+        <extracomment>The name of the ParamType of StateType (57f1b7cc-26c8-434b-ba04-d3077dc886c8) of DeviceClass gpioInputRpi
 ----------
-The name of the ParamType of StateType (57f1b7cc-26c8-434b-ba04-d3077dc886c8) of DeviceClass gpioButtonBbb</extracomment>
+The name of the ParamType of StateType (57f1b7cc-26c8-434b-ba04-d3077dc886c8) of DeviceClass gpioInputBbb</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
@@ -99,15 +108,6 @@ The name of the ParamType of StateType (57f1b7cc-26c8-434b-ba04-d3077dc886c8) of
         <location filename="../plugininfo.h" line="123"/>
         <source>Set power</source>
         <extracomment>The name of the autocreated ActionType (06843766-358e-44b0-8d52-2b46ef98459a)</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="84"/>
-        <location filename="../plugininfo.h" line="126"/>
-        <source>GPIO Button</source>
-        <extracomment>The name of the DeviceClass (6aff228b-0410-4ef9-9593-51e8639aacea)
-----------
-The name of the DeviceClass (ffd2aa29-55cc-4824-ba95-c311784f7824)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
