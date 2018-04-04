89ce1f4819660cc07bfc1d528a3ce77e91d652c5
Jenkins <jenkins@nymea.io>
2018-04-04 16:31:39 +0200

    Automatic translations update by Jenkins

--- netatmo/translations/69d14951-0c02-4877-bcef-dffdf48b7ccb-de_DE.ts
+++ netatmo/translations/69d14951-0c02-4877-bcef-dffdf48b7ccb-de_DE.ts
@@ -4,8 +4,8 @@
 <context>
     <name>Netatmo</name>
     <message>
-        <location filename="../plugininfo.h" line="89"/>
-        <location filename="../plugininfo.h" line="92"/>
+        <location filename="../plugininfo.h" line="87"/>
+        <location filename="../plugininfo.h" line="90"/>
         <source>Netatmo</source>
         <extracomment>The name of the plugin Netatmo (69d14951-0c02-4877-bcef-dffdf48b7ccb)
 ----------
@@ -13,14 +13,14 @@ The name of the vendor (4b46b4ed-5ec9-4aa4-afc3-92d3f80e6351)</extracomment>
         <translation>Netatmo</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="95"/>
+        <location filename="../plugininfo.h" line="93"/>
         <source>Netatmo Connection</source>
         <extracomment>The name of the DeviceClass (728d5a67-27a3-400e-b83c-2765f5196f69)</extracomment>
         <translation>Netatmo Verbindung</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="113"/>
-        <location filename="../plugininfo.h" line="179"/>
+        <location filename="../plugininfo.h" line="111"/>
+        <location filename="../plugininfo.h" line="174"/>
         <source>name</source>
         <extracomment>The name of the paramType (a97d256c-e159-4aa0-bc71-6bd7cd0688b3) of indoor
 ----------
@@ -28,38 +28,38 @@ The name of the paramType (a97d256c-e159-4aa0-bc71-6bd7cd0688b3) of outdoor</ext
         <translation>Name</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="98"/>
+        <location filename="../plugininfo.h" line="96"/>
         <source>username</source>
         <extracomment>The name of the paramType (763c2c10-dee5-41c8-9f7e-ded741945e73) of connection</extracomment>
         <translation>Benutzername</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="101"/>
+        <location filename="../plugininfo.h" line="99"/>
         <source>password</source>
         <extracomment>The name of the paramType (c0d892d6-f359-4782-9d7d-8f74a3b53e3e) of connection</extracomment>
         <translation>Passwort</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="104"/>
+        <location filename="../plugininfo.h" line="102"/>
         <source>available changed</source>
         <extracomment>The name of the autocreated EventType (2f79bc1d-27ed-480a-b583-728363c83ea6)</extracomment>
         <translation>Verfügbarkeit geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="107"/>
+        <location filename="../plugininfo.h" line="105"/>
         <source>available</source>
         <extracomment>The name of the ParamType of StateType (2f79bc1d-27ed-480a-b583-728363c83ea6) of DeviceClass connection</extracomment>
         <translation>Verfügbar</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="110"/>
+        <location filename="../plugininfo.h" line="108"/>
         <source>Indoor Station</source>
         <extracomment>The name of the DeviceClass (1c809049-04f2-4710-99f5-6ed379a2934f)</extracomment>
         <translation>Innenstation</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="116"/>
-        <location filename="../plugininfo.h" line="182"/>
+        <location filename="../plugininfo.h" line="114"/>
+        <location filename="../plugininfo.h" line="177"/>
         <source>mac address</source>
         <extracomment>The name of the paramType (157d470a-e579-4d0e-b879-6b5bfa8e34ae) of indoor
 ----------
@@ -67,31 +67,22 @@ The name of the paramType (157d470a-e579-4d0e-b879-6b5bfa8e34ae) of outdoor</ext
         <translation>MAC Adresse</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="119"/>
-        <location filename="../plugininfo.h" line="185"/>
-        <source>connection</source>
-        <extracomment>The name of the paramType (c15e5dea-10f2-4c96-afab-649872f43d5c) of indoor
-----------
-The name of the paramType (c15e5dea-10f2-4c96-afab-649872f43d5c) of outdoor</extracomment>
-        <translation>Verbindung</translation>
-    </message>
-    <message>
-        <location filename="../plugininfo.h" line="122"/>
-        <location filename="../plugininfo.h" line="191"/>
+        <location filename="../plugininfo.h" line="117"/>
+        <location filename="../plugininfo.h" line="183"/>
         <source>last update changed</source>
         <extracomment>The name of the autocreated EventType (50da9f6b-c350-401c-a72e-2e4036f3975d)</extracomment>
         <translation>Letztes Update geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="128"/>
-        <location filename="../plugininfo.h" line="197"/>
+        <location filename="../plugininfo.h" line="123"/>
+        <location filename="../plugininfo.h" line="189"/>
         <source>temperature changed</source>
         <extracomment>The name of the autocreated EventType (3cb25538-e463-40ae-92f9-8f34f0c06b92)</extracomment>
         <translation>Temperatur geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="131"/>
-        <location filename="../plugininfo.h" line="200"/>
+        <location filename="../plugininfo.h" line="126"/>
+        <location filename="../plugininfo.h" line="192"/>
         <source>temperature</source>
         <extracomment>The name of the ParamType of StateType (3cb25538-e463-40ae-92f9-8f34f0c06b92) of DeviceClass indoor
 ----------
@@ -99,22 +90,22 @@ The name of the ParamType of StateType (3cb25538-e463-40ae-92f9-8f34f0c06b92) of
         <translation>Temperatur</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="134"/>
-        <location filename="../plugininfo.h" line="203"/>
+        <location filename="../plugininfo.h" line="129"/>
+        <location filename="../plugininfo.h" line="195"/>
         <source>temperature minimum changed</source>
         <extracomment>The name of the autocreated EventType (ae8bb713-8805-4efd-89a1-bca44a1f1690)</extracomment>
         <translation>Temperatur Minimum geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="140"/>
-        <location filename="../plugininfo.h" line="209"/>
+        <location filename="../plugininfo.h" line="135"/>
+        <location filename="../plugininfo.h" line="201"/>
         <source>temperature maximum changed</source>
         <extracomment>The name of the autocreated EventType (dd30507e-037b-4c74-bcca-e04b94c7c5fe)</extracomment>
         <translation>Temperatur Maximum geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="125"/>
-        <location filename="../plugininfo.h" line="194"/>
+        <location filename="../plugininfo.h" line="120"/>
+        <location filename="../plugininfo.h" line="186"/>
         <source>updateTime</source>
         <extracomment>The name of the ParamType of StateType (50da9f6b-c350-401c-a72e-2e4036f3975d) of DeviceClass indoor
 ----------
@@ -122,8 +113,8 @@ The name of the ParamType of StateType (50da9f6b-c350-401c-a72e-2e4036f3975d) of
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="137"/>
-        <location filename="../plugininfo.h" line="206"/>
+        <location filename="../plugininfo.h" line="132"/>
+        <location filename="../plugininfo.h" line="198"/>
         <source>temperatureMin</source>
         <extracomment>The name of the ParamType of StateType (ae8bb713-8805-4efd-89a1-bca44a1f1690) of DeviceClass indoor
 ----------
@@ -131,8 +122,8 @@ The name of the ParamType of StateType (ae8bb713-8805-4efd-89a1-bca44a1f1690) of
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="143"/>
-        <location filename="../plugininfo.h" line="212"/>
+        <location filename="../plugininfo.h" line="138"/>
+        <location filename="../plugininfo.h" line="204"/>
         <source>temperatureMax</source>
         <extracomment>The name of the ParamType of StateType (dd30507e-037b-4c74-bcca-e04b94c7c5fe) of DeviceClass indoor
 ----------
@@ -140,15 +131,15 @@ The name of the ParamType of StateType (dd30507e-037b-4c74-bcca-e04b94c7c5fe) of
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="146"/>
-        <location filename="../plugininfo.h" line="215"/>
+        <location filename="../plugininfo.h" line="141"/>
+        <location filename="../plugininfo.h" line="207"/>
         <source>humidity changed</source>
         <extracomment>The name of the autocreated EventType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9)</extracomment>
         <translation>Luftfeuchtigkeit geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="149"/>
-        <location filename="../plugininfo.h" line="218"/>
+        <location filename="../plugininfo.h" line="144"/>
+        <location filename="../plugininfo.h" line="210"/>
         <source>humidity</source>
         <extracomment>The name of the ParamType of StateType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9) of DeviceClass indoor
 ----------
@@ -156,85 +147,85 @@ The name of the ParamType of StateType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9) of
         <translation>Luftfeuchtigkeit</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="152"/>
+        <location filename="../plugininfo.h" line="147"/>
         <source>pressure changed</source>
         <extracomment>The name of the autocreated EventType (03b0a7b7-987d-4d3b-b3f0-21d9f92ad326)</extracomment>
         <translation>Luftdruck geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="155"/>
+        <location filename="../plugininfo.h" line="150"/>
         <source>pressure</source>
         <extracomment>The name of the ParamType of StateType (03b0a7b7-987d-4d3b-b3f0-21d9f92ad326) of DeviceClass indoor</extracomment>
         <translation>Luftdruck</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="158"/>
+        <location filename="../plugininfo.h" line="153"/>
         <source>noise changed</source>
         <extracomment>The name of the autocreated EventType (906cea9d-1daf-4e9c-90b9-e40f43052a34)</extracomment>
         <translation>Geräuschpegel geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="161"/>
+        <location filename="../plugininfo.h" line="156"/>
         <source>noise</source>
         <extracomment>The name of the ParamType of StateType (906cea9d-1daf-4e9c-90b9-e40f43052a34) of DeviceClass indoor</extracomment>
         <translation>Geräuschpegel</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="164"/>
+        <location filename="../plugininfo.h" line="159"/>
         <source>co2 changed</source>
         <extracomment>The name of the autocreated EventType (e5710bd1-79fa-4bd4-9052-8416aae909b9)</extracomment>
         <translation>CO2 Wert geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="167"/>
+        <location filename="../plugininfo.h" line="162"/>
         <source>co2</source>
         <extracomment>The name of the ParamType of StateType (e5710bd1-79fa-4bd4-9052-8416aae909b9) of DeviceClass indoor</extracomment>
         <translation>CO2 Wert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="170"/>
+        <location filename="../plugininfo.h" line="165"/>
         <source>wifi signal strength changed</source>
         <extracomment>The name of the autocreated EventType (6ea906d4-5740-454d-a730-6fdb9fa0d624)</extracomment>
         <translation>Wifi Signalstärke geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="173"/>
+        <location filename="../plugininfo.h" line="168"/>
         <source>wifiStrength</source>
         <extracomment>The name of the ParamType of StateType (6ea906d4-5740-454d-a730-6fdb9fa0d624) of DeviceClass indoor</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="224"/>
+        <location filename="../plugininfo.h" line="216"/>
         <source>signalStrength</source>
         <extracomment>The name of the ParamType of StateType (0faa3d08-9004-46fb-a5aa-a59b75e454cc) of DeviceClass outdoor</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="176"/>
+        <location filename="../plugininfo.h" line="171"/>
         <source>Outdoor Station</source>
         <extracomment>The name of the DeviceClass (6cc01d62-7317-4ec4-8ac4-a4cab762c179)</extracomment>
         <translation>Außenstation</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="188"/>
+        <location filename="../plugininfo.h" line="180"/>
         <source>base station</source>
         <extracomment>The name of the paramType (d7a0ec46-760c-4fdc-9753-fe10c86fe1b9) of outdoor</extracomment>
         <translation>Basisstation</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="221"/>
+        <location filename="../plugininfo.h" line="213"/>
         <source>signal strength changed</source>
         <extracomment>The name of the autocreated EventType (0faa3d08-9004-46fb-a5aa-a59b75e454cc)</extracomment>
         <translation>Signalstärke geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="227"/>
+        <location filename="../plugininfo.h" line="219"/>
         <source>battery changed</source>
         <extracomment>The name of the autocreated EventType (15d8fae1-ba47-42e1-994d-530e8017c965)</extracomment>
         <translation>Batteriestatus geändert</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="230"/>
+        <location filename="../plugininfo.h" line="222"/>
         <source>battery</source>
         <extracomment>The name of the ParamType of StateType (15d8fae1-ba47-42e1-994d-530e8017c965) of DeviceClass outdoor</extracomment>
         <translation>Batterie</translation>
