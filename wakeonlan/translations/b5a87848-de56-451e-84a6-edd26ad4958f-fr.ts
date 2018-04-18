d1da5227fba986cb4b19ca6571331ffe3fdd2ffb
Christian Warejko <c.warejko@marantec.de>
2018-04-18 15:28:25 +0000

    Translated using Weblate (French)
    Currently translated at 100.0% (5 of 5 strings)
    
    Translation: nymea-plugins/wakeonlan
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/wakeonlan/fr/

--- wakeonlan/translations/b5a87848-de56-451e-84a6-edd26ad4958f-fr.ts
+++ wakeonlan/translations/b5a87848-de56-451e-84a6-edd26ad4958f-fr.ts
@@ -19,19 +19,19 @@
         <location filename="../plugininfo.h" line="35"/>
         <source>Wake On Lan</source>
         <extracomment>The name of the DeviceClass (3c8f2447-dcd0-4882-8c09-99e579e4d24c)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Wake on LAN</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="38"/>
         <source>mac</source>
         <extracomment>The name of the paramType (e2ba04ac-9fe1-4f9d-8152-024c27030cde) of wol</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>MAC adresse</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="41"/>
         <source>wake up device</source>
         <extracomment>The name of the ActionType fb9b9d87-218f-4f0d-9e16-39f8a105029a of deviceClass wol</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Réveiller l&apos;appareil</translation>
     </message>
 </context>
 </TS>
