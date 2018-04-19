ecb0d2a0d4a2c9061091a45c824cb3ea813bf0b9
Jenkins <jenkins@nymea.io>
2018-04-19 14:05:31 +0200

    Automatic translations update by Jenkins

--- dweetio/translations/9338f22c-df47-414e-a33c-9b6d40c953c9-en_US.ts
+++ dweetio/translations/9338f22c-df47-414e-a33c-9b6d40c953c9-en_US.ts
@@ -1,4 +1,80 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+<context>
+    <name>Dweetio</name>
+    <message>
+        <source>Dweetio</source>
+        <extracomment>The name of the plugin Dweetio (9338f22c-df47-414e-a33c-9b6d40c953c9)
+----------
+The name of the vendor (23ff381d-c580-4ba1-85bd-30598f893a43)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Post</source>
+        <extracomment>The name of the DeviceClass (5036030e-adc7-403b-b9b6-a27a4a5afea8)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Thing</source>
+        <extracomment>The name of the paramType (b50b3682-fb2e-4185-b7aa-31741648d9bb) of post
+----------
+The name of the paramType (b50b3682-fb2e-4185-b7aa-31741648d9bb) of get</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>key</source>
+        <extracomment>The name of the paramType (892b7323-ee85-4520-9160-f206f11b344e) of post</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content Name</source>
+        <extracomment>The name of the paramType (f2b2c3ae-a915-4dd9-a9dd-9545d9dd3d4e) of post</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>connected changed</source>
+        <extracomment>The name of the autocreated EventType (4d1790bf-28c6-4c1f-8892-ba1a0ef140f5)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>connected</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (4d1790bf-28c6-4c1f-8892-ba1a0ef140f5) of DeviceClass post
+----------
+The name of the ParamType of the autocreated EventType (4d1790bf-28c6-4c1f-8892-ba1a0ef140f5) of DeviceClass get</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Send Content</source>
+        <extracomment>The name of the ActionType 76472328-dbaa-458f-90c9-0b46968af511 of deviceClass post</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content</source>
+        <extracomment>The name of the paramType (13760d36-26eb-40b5-85b3-450ca7937666) of post
+----------
+The name of the ParamType of the autocreated EventType (53dfe2bc-792d-420e-bc1f-2890ad715b27) of DeviceClass get</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Get</source>
+        <extracomment>The name of the DeviceClass (8318f86c-061a-450f-978d-f42e3513ca8c)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Key</source>
+        <extracomment>The name of the paramType (892b7323-ee85-4520-9160-f206f11b344e) of get</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Content name</source>
+        <extracomment>The name of the paramType (f2b2c3ae-a915-4dd9-a9dd-9545d9dd3d4e) of get</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>content changed</source>
+        <extracomment>The name of the autocreated EventType (53dfe2bc-792d-420e-bc1f-2890ad715b27)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
