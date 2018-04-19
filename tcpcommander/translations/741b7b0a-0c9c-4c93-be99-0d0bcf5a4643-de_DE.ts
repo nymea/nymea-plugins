b65d6334fef292a7d0a5a8bf191debbd4e60008e
Bernhard Trinnes <bernhard.trinnes@guh.io>
2018-04-19 12:59:58 +0000

    Translated using Weblate (German)
    Currently translated at 86.6% (13 of 15 strings)
    
    Translation: nymea-plugins/tcpcommander
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/tcpcommander/de/

--- tcpcommander/translations/741b7b0a-0c9c-4c93-be99-0d0bcf5a4643-de_DE.ts
+++ tcpcommander/translations/741b7b0a-0c9c-4c93-be99-0d0bcf5a4643-de_DE.ts
@@ -19,13 +19,13 @@
         <location filename="../plugininfo.h" line="51"/>
         <source>TCP Output</source>
         <extracomment>The name of the DeviceClass (c67d059f-694f-47cb-8e1d-9e3e6d014c1a)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>TCP Ausgang</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="54"/>
         <source>IPv4 Address</source>
         <extracomment>The name of the paramType (2a3fcb23-931b-4ba1-b134-c49b656c76f7) of tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>IPv4 Adresse</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="57"/>
@@ -34,14 +34,14 @@
         <extracomment>The name of the paramType (bee8b151-815a-4159-9d8a-42b76e99b42c) of tcpOutput
 ----------
 The name of the paramType (bee8b151-815a-4159-9d8a-42b76e99b42c) of tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="60"/>
         <location filename="../plugininfo.h" line="84"/>
         <source>connection status changed</source>
         <extracomment>The name of the autocreated EventType (725b541a-9e0c-4634-81eb-e415c0b8f025)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbindungsstatus geändert</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="63"/>
@@ -50,25 +50,25 @@ The name of the paramType (bee8b151-815a-4159-9d8a-42b76e99b42c) of tcpInput</ex
         <extracomment>The name of the ParamType of the autocreated EventType (725b541a-9e0c-4634-81eb-e415c0b8f025) of DeviceClass tcpOutput
 ----------
 The name of the ParamType of the autocreated EventType (725b541a-9e0c-4634-81eb-e415c0b8f025) of DeviceClass tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="66"/>
         <source>Send Data</source>
         <extracomment>The name of the ActionType 6bc52462-b192-46a4-a6df-92cc5a479c89 of deviceClass tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Sende Daten</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="69"/>
         <source>Data</source>
         <extracomment>The name of the paramType (6604c852-6b24-4707-b8e5-1ddd8032efcc) of tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Daten</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="72"/>
         <source>TCP Input</source>
         <extracomment>The name of the DeviceClass (bc40e84a-69c4-4fd2-beb0-bd65f19aa8ff)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>TCP Eingang</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="78"/>
@@ -80,19 +80,19 @@ The name of the ParamType of the autocreated EventType (725b541a-9e0c-4634-81eb-
         <location filename="../plugininfo.h" line="81"/>
         <source>Command</source>
         <extracomment>The name of the paramType (23051bdf-3f50-41fa-abde-bc4fe0bcc4fc) of tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kommando</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="90"/>
         <source>Data received</source>
         <extracomment>The name of the autocreated EventType (b98fdacc-59d7-41c4-b790-1fdca50dfb22)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Daten empfangen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="93"/>
         <source>Data Received</source>
         <extracomment>The name of the ParamType of the autocreated EventType (b98fdacc-59d7-41c4-b790-1fdca50dfb22) of DeviceClass tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Daten Empfangen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="96"/>
