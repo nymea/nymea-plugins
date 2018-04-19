1a5c5326bb047ec62ca4a05b16a8a848bfaa4abe
Jenkins <jenkins@nymea.io>
2018-04-19 12:05:02 +0200

    Automatic translations update by Jenkins

--- elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-de_DE.ts
+++ elgato/translations/c5c03ad4-bfdb-444a-8eca-2c234c46cc27-de_DE.ts
@@ -4,8 +4,8 @@
 <context>
     <name>Elgato</name>
     <message>
-        <location filename="../plugininfo.h" line="72"/>
-        <location filename="../plugininfo.h" line="75"/>
+        <location filename="../plugininfo.h" line="77"/>
+        <location filename="../plugininfo.h" line="80"/>
         <source>Elgato</source>
         <extracomment>The name of the plugin Elgato (c5c03ad4-bfdb-444a-8eca-2c234c46cc27)
 ----------
@@ -13,50 +13,50 @@ The name of the vendor (90a3091d-1053-4f77-8dc3-92e27bbcebe7)</extracomment>
         <translation>Elgato</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="78"/>
+        <location filename="../plugininfo.h" line="83"/>
         <source>Avea</source>
         <extracomment>The name of the DeviceClass (164f9602-90ee-4693-bda3-9cafae37603e)</extracomment>
         <translation>Avea</translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="123"/>
+        <location filename="../plugininfo.h" line="128"/>
         <source>Set color</source>
         <extracomment>The name of the autocreated ActionType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="81"/>
+        <location filename="../plugininfo.h" line="86"/>
         <source>Name</source>
         <extracomment>The name of the paramType (d7a5d96f-33dc-4dc3-a171-b1e89a5e1a4b) of avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="84"/>
+        <location filename="../plugininfo.h" line="89"/>
         <source>MAC address</source>
         <extracomment>The name of the paramType (20932e39-00fd-40e3-8f39-850ba149844e) of avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="87"/>
+        <location filename="../plugininfo.h" line="92"/>
         <source>Connected changed</source>
         <extracomment>The name of the autocreated EventType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="90"/>
+        <location filename="../plugininfo.h" line="95"/>
         <source>Connected</source>
         <extracomment>The name of the ParamType of the autocreated EventType (6d5e792a-c786-40d2-ae35-a48ac6fafcbc) of DeviceClass avea</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="93"/>
+        <location filename="../plugininfo.h" line="98"/>
         <source>Power changed</source>
         <extracomment>The name of the autocreated EventType (2b779968-ff4f-4a05-a466-074e14288a8c)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="96"/>
-        <location filename="../plugininfo.h" line="102"/>
+        <location filename="../plugininfo.h" line="101"/>
+        <location filename="../plugininfo.h" line="107"/>
         <source>Power</source>
         <extracomment>The name of the ParamType of the autocreated EventType (2b779968-ff4f-4a05-a466-074e14288a8c) of DeviceClass avea
 ----------
@@ -64,8 +64,8 @@ The name of the autocreated ParamType of the writable StateType (2b779968-ff4f-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="108"/>
-        <location filename="../plugininfo.h" line="114"/>
+        <location filename="../plugininfo.h" line="113"/>
+        <location filename="../plugininfo.h" line="119"/>
         <source>Brightness</source>
         <extracomment>The name of the ParamType of the autocreated EventType (c38181a0-e19b-423f-8b66-dedda94e89b5) of DeviceClass avea
 ----------
@@ -73,8 +73,8 @@ The name of the autocreated ParamType of the writable StateType (c38181a0-e19b-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="120"/>
-        <location filename="../plugininfo.h" line="126"/>
+        <location filename="../plugininfo.h" line="125"/>
+        <location filename="../plugininfo.h" line="131"/>
         <source>Color</source>
         <extracomment>The name of the ParamType of the autocreated EventType (3f15637a-8272-4714-bd08-04806e05bbef) of DeviceClass avea
 ----------
@@ -82,8 +82,8 @@ The name of the autocreated ParamType of the writable StateType (3f15637a-8272-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="144"/>
-        <location filename="../plugininfo.h" line="150"/>
+        <location filename="../plugininfo.h" line="161"/>
+        <location filename="../plugininfo.h" line="167"/>
         <source>Red</source>
         <extracomment>The name of the ParamType of the autocreated EventType (50ab71d1-8970-4531-b1ad-f387d01bab90) of DeviceClass avea
 ----------
@@ -91,8 +91,8 @@ The name of the autocreated ParamType of the writable StateType (50ab71d1-8970-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="156"/>
-        <location filename="../plugininfo.h" line="162"/>
+        <location filename="../plugininfo.h" line="173"/>
+        <location filename="../plugininfo.h" line="179"/>
         <source>Green</source>
         <extracomment>The name of the ParamType of the autocreated EventType (9e8ddebc-33fe-418a-895f-d8148fabcfee) of DeviceClass avea
 ----------
@@ -100,8 +100,8 @@ The name of the autocreated ParamType of the writable StateType (9e8ddebc-33fe-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="168"/>
-        <location filename="../plugininfo.h" line="174"/>
+        <location filename="../plugininfo.h" line="185"/>
+        <location filename="../plugininfo.h" line="191"/>
         <source>Blue</source>
         <extracomment>The name of the ParamType of the autocreated EventType (f4310747-d468-4d2e-abe4-b5e4d0245dad) of DeviceClass avea
 ----------
@@ -109,8 +109,8 @@ The name of the autocreated ParamType of the writable StateType (f4310747-d468-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="180"/>
-        <location filename="../plugininfo.h" line="186"/>
+        <location filename="../plugininfo.h" line="197"/>
+        <location filename="../plugininfo.h" line="203"/>
         <source>Fade</source>
         <extracomment>The name of the ParamType of the autocreated EventType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d) of DeviceClass avea
 ----------
@@ -118,38 +118,59 @@ The name of the autocreated ParamType of the writable StateType (01ccda48-44c6-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="99"/>
+        <location filename="../plugininfo.h" line="104"/>
         <source>Set power</source>
         <extracomment>The name of the autocreated ActionType (2b779968-ff4f-4a05-a466-074e14288a8c)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="105"/>
+        <location filename="../plugininfo.h" line="110"/>
         <source>Brightness changed</source>
         <extracomment>The name of the autocreated EventType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="111"/>
+        <location filename="../plugininfo.h" line="116"/>
         <source>Set brightness</source>
         <extracomment>The name of the autocreated ActionType (c38181a0-e19b-423f-8b66-dedda94e89b5)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="117"/>
+        <location filename="../plugininfo.h" line="122"/>
         <source>Color changed</source>
         <extracomment>The name of the autocreated EventType (3f15637a-8272-4714-bd08-04806e05bbef)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="129"/>
+        <location filename="../plugininfo.h" line="134"/>
+        <source>color temperature changed</source>
+        <extracomment>The name of the autocreated EventType (8932ddac-d718-4345-94aa-f4c9611f132f)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="137"/>
+        <location filename="../plugininfo.h" line="143"/>
+        <source>color temperature</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (8932ddac-d718-4345-94aa-f4c9611f132f) of DeviceClass avea
+----------
+The name of the autocreated ParamType of the writable StateType (8932ddac-d718-4345-94aa-f4c9611f132f) of DeviceClass avea</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="140"/>
+        <source>Set color temperature</source>
+        <extracomment>The name of the autocreated ActionType (8932ddac-d718-4345-94aa-f4c9611f132f)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../plugininfo.h" line="146"/>
         <source>white changed</source>
         <extracomment>The name of the autocreated EventType (50f6a224-fb8d-487d-8774-1a0536e5b1aa)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="132"/>
-        <location filename="../plugininfo.h" line="138"/>
+        <location filename="../plugininfo.h" line="149"/>
+        <location filename="../plugininfo.h" line="155"/>
         <source>white</source>
         <extracomment>The name of the ParamType of the autocreated EventType (50f6a224-fb8d-487d-8774-1a0536e5b1aa) of DeviceClass avea
 ----------
@@ -157,55 +178,55 @@ The name of the autocreated ParamType of the writable StateType (50f6a224-fb8d-4
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="135"/>
+        <location filename="../plugininfo.h" line="152"/>
         <source>Set white</source>
         <extracomment>The name of the autocreated ActionType (50f6a224-fb8d-487d-8774-1a0536e5b1aa)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="141"/>
+        <location filename="../plugininfo.h" line="158"/>
         <source>Red changed</source>
         <extracomment>The name of the autocreated EventType (50ab71d1-8970-4531-b1ad-f387d01bab90)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="147"/>
+        <location filename="../plugininfo.h" line="164"/>
         <source>Set red</source>
         <extracomment>The name of the autocreated ActionType (50ab71d1-8970-4531-b1ad-f387d01bab90)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="153"/>
+        <location filename="../plugininfo.h" line="170"/>
         <source>Green changed</source>
         <extracomment>The name of the autocreated EventType (9e8ddebc-33fe-418a-895f-d8148fabcfee)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="159"/>
+        <location filename="../plugininfo.h" line="176"/>
         <source>Set green</source>
         <extracomment>The name of the autocreated ActionType (9e8ddebc-33fe-418a-895f-d8148fabcfee)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="165"/>
+        <location filename="../plugininfo.h" line="182"/>
         <source>Blue changed</source>
         <extracomment>The name of the autocreated EventType (f4310747-d468-4d2e-abe4-b5e4d0245dad)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="171"/>
+        <location filename="../plugininfo.h" line="188"/>
         <source>Set blue</source>
         <extracomment>The name of the autocreated ActionType (f4310747-d468-4d2e-abe4-b5e4d0245dad)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="177"/>
+        <location filename="../plugininfo.h" line="194"/>
         <source>Fade changed</source>
         <extracomment>The name of the autocreated EventType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d)</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../plugininfo.h" line="183"/>
+        <location filename="../plugininfo.h" line="200"/>
         <source>Set fade</source>
         <extracomment>The name of the autocreated ActionType (01ccda48-44c6-495b-b0a7-c2a0bf5a570d)</extracomment>
         <translation type="unfinished"></translation>
