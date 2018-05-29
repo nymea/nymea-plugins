51f991da645dd7afcf53a041276f25689beffe9f
Bernhard Trinnes <bernhard.trinnes@guh.io>
2018-05-29 13:28:25 +0000

    Translated using Weblate (Czech)
    Currently translated at 100.0% (33 of 33 strings)
    
    Translation: nymea-plugins/netatmo
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/netatmo/cs/

--- netatmo/translations/69d14951-0c02-4877-bcef-dffdf48b7ccb-cs.ts
+++ netatmo/translations/69d14951-0c02-4877-bcef-dffdf48b7ccb-cs.ts
@@ -16,7 +16,7 @@ The name of the vendor (4b46b4ed-5ec9-4aa4-afc3-92d3f80e6351)</extracomment>
         <location filename="../plugininfo.h" line="93"/>
         <source>Netatmo Connection</source>
         <extracomment>The name of the DeviceClass (728d5a67-27a3-400e-b83c-2765f5196f69)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Netatmo připojení</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="111"/>
@@ -25,37 +25,37 @@ The name of the vendor (4b46b4ed-5ec9-4aa4-afc3-92d3f80e6351)</extracomment>
         <extracomment>The name of the paramType (a97d256c-e159-4aa0-bc71-6bd7cd0688b3) of indoor
 ----------
 The name of the paramType (a97d256c-e159-4aa0-bc71-6bd7cd0688b3) of outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>název</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="96"/>
         <source>username</source>
         <extracomment>The name of the paramType (763c2c10-dee5-41c8-9f7e-ded741945e73) of connection</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>název uživatele</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="99"/>
         <source>password</source>
         <extracomment>The name of the paramType (c0d892d6-f359-4782-9d7d-8f74a3b53e3e) of connection</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>heslo</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="102"/>
         <source>available changed</source>
         <extracomment>The name of the autocreated EventType (2f79bc1d-27ed-480a-b583-728363c83ea6)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>k dispozici změněno</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="105"/>
         <source>available</source>
         <extracomment>The name of the ParamType of the autocreated EventType (2f79bc1d-27ed-480a-b583-728363c83ea6) of DeviceClass connection</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>k dispozici</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="108"/>
         <source>Indoor Station</source>
         <extracomment>The name of the DeviceClass (1c809049-04f2-4710-99f5-6ed379a2934f)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Vnitřní stanice</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="114"/>
@@ -64,14 +64,14 @@ The name of the paramType (a97d256c-e159-4aa0-bc71-6bd7cd0688b3) of outdoor</ext
         <extracomment>The name of the paramType (157d470a-e579-4d0e-b879-6b5bfa8e34ae) of indoor
 ----------
 The name of the paramType (157d470a-e579-4d0e-b879-6b5bfa8e34ae) of outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>mac adresa</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="117"/>
         <location filename="../plugininfo.h" line="183"/>
         <source>last update changed</source>
         <extracomment>The name of the autocreated EventType (50da9f6b-c350-401c-a72e-2e4036f3975d)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>poslední aktualizace změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="120"/>
@@ -80,14 +80,14 @@ The name of the paramType (157d470a-e579-4d0e-b879-6b5bfa8e34ae) of outdoor</ext
         <extracomment>The name of the ParamType of the autocreated EventType (50da9f6b-c350-401c-a72e-2e4036f3975d) of DeviceClass indoor
 ----------
 The name of the ParamType of the autocreated EventType (50da9f6b-c350-401c-a72e-2e4036f3975d) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>poslední aktualizace</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="123"/>
         <location filename="../plugininfo.h" line="189"/>
         <source>temperature changed</source>
         <extracomment>The name of the autocreated EventType (3cb25538-e463-40ae-92f9-8f34f0c06b92)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>teplota změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="126"/>
@@ -96,14 +96,14 @@ The name of the ParamType of the autocreated EventType (50da9f6b-c350-401c-a72e-
         <extracomment>The name of the ParamType of the autocreated EventType (3cb25538-e463-40ae-92f9-8f34f0c06b92) of DeviceClass indoor
 ----------
 The name of the ParamType of the autocreated EventType (3cb25538-e463-40ae-92f9-8f34f0c06b92) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>teplota</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="129"/>
         <location filename="../plugininfo.h" line="195"/>
         <source>temperature minimum changed</source>
         <extracomment>The name of the autocreated EventType (ae8bb713-8805-4efd-89a1-bca44a1f1690)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>minimální teplota změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="132"/>
@@ -112,14 +112,14 @@ The name of the ParamType of the autocreated EventType (3cb25538-e463-40ae-92f9-
         <extracomment>The name of the ParamType of the autocreated EventType (ae8bb713-8805-4efd-89a1-bca44a1f1690) of DeviceClass indoor
 ----------
 The name of the ParamType of the autocreated EventType (ae8bb713-8805-4efd-89a1-bca44a1f1690) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>minimální teplota</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="135"/>
         <location filename="../plugininfo.h" line="201"/>
         <source>temperature maximum changed</source>
         <extracomment>The name of the autocreated EventType (dd30507e-037b-4c74-bcca-e04b94c7c5fe)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>maximální teplota změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="138"/>
@@ -128,20 +128,20 @@ The name of the ParamType of the autocreated EventType (ae8bb713-8805-4efd-89a1-
         <extracomment>The name of the ParamType of the autocreated EventType (dd30507e-037b-4c74-bcca-e04b94c7c5fe) of DeviceClass indoor
 ----------
 The name of the ParamType of the autocreated EventType (dd30507e-037b-4c74-bcca-e04b94c7c5fe) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>maximální teplota</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="168"/>
         <source>wifi signal strength</source>
         <extracomment>The name of the ParamType of the autocreated EventType (6ea906d4-5740-454d-a730-6fdb9fa0d624) of DeviceClass indoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>wifi signál síla</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="141"/>
         <location filename="../plugininfo.h" line="207"/>
         <source>humidity changed</source>
         <extracomment>The name of the autocreated EventType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>vlhkost změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="144"/>
@@ -150,85 +150,85 @@ The name of the ParamType of the autocreated EventType (dd30507e-037b-4c74-bcca-
         <extracomment>The name of the ParamType of the autocreated EventType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9) of DeviceClass indoor
 ----------
 The name of the ParamType of the autocreated EventType (e2db5f01-196a-48d1-8874-6b8cbfe0d8c9) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>vlhkost</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="147"/>
         <source>pressure changed</source>
         <extracomment>The name of the autocreated EventType (03b0a7b7-987d-4d3b-b3f0-21d9f92ad326)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>tlak změněn</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="150"/>
         <source>pressure</source>
         <extracomment>The name of the ParamType of the autocreated EventType (03b0a7b7-987d-4d3b-b3f0-21d9f92ad326) of DeviceClass indoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>tlak</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="153"/>
         <source>noise changed</source>
         <extracomment>The name of the autocreated EventType (906cea9d-1daf-4e9c-90b9-e40f43052a34)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>zvuk změněn</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="156"/>
         <source>noise</source>
         <extracomment>The name of the ParamType of the autocreated EventType (906cea9d-1daf-4e9c-90b9-e40f43052a34) of DeviceClass indoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>zvuk</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="159"/>
         <source>co2 changed</source>
         <extracomment>The name of the autocreated EventType (e5710bd1-79fa-4bd4-9052-8416aae909b9)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>co2 změněn</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="162"/>
         <source>co2</source>
         <extracomment>The name of the ParamType of the autocreated EventType (e5710bd1-79fa-4bd4-9052-8416aae909b9) of DeviceClass indoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>co2</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="165"/>
         <source>wifi signal strength changed</source>
         <extracomment>The name of the autocreated EventType (6ea906d4-5740-454d-a730-6fdb9fa0d624)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>wifi signál síla změněno</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="171"/>
         <source>Outdoor Station</source>
         <extracomment>The name of the DeviceClass (6cc01d62-7317-4ec4-8ac4-a4cab762c179)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Venkovní stanice</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="180"/>
         <source>base station</source>
         <extracomment>The name of the paramType (d7a0ec46-760c-4fdc-9753-fe10c86fe1b9) of outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>základní stanice</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="213"/>
         <source>signal strength changed</source>
         <extracomment>The name of the autocreated EventType (0faa3d08-9004-46fb-a5aa-a59b75e454cc)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>signál síla změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="216"/>
         <source>signal strength</source>
         <extracomment>The name of the ParamType of the autocreated EventType (0faa3d08-9004-46fb-a5aa-a59b75e454cc) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>signál síla</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="219"/>
         <source>battery changed</source>
         <extracomment>The name of the autocreated EventType (15d8fae1-ba47-42e1-994d-530e8017c965)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>baterie změněna</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="222"/>
         <source>battery</source>
         <extracomment>The name of the ParamType of the autocreated EventType (15d8fae1-ba47-42e1-994d-530e8017c965) of DeviceClass outdoor</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>baterie</translation>
     </message>
 </context>
 </TS>
