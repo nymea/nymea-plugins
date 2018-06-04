d70b7f718ec941859265252120eea204b4e782e2
Bernhard Trinnes <bernhard.trinnes@guh.io>
2018-06-04 07:56:01 +0000

    Translated using Weblate (Spanish)
    Currently translated at 100.0% (23 of 23 strings)
    
    Translation: nymea-plugins/senic
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/senic/es/

--- senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-es.ts
+++ senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-es.ts
@@ -16,133 +16,133 @@ The name of the vendor (8e6cae9c-706b-49e5-ae86-da52a20dd336)</extracomment>
         <location filename="../plugininfo.h" line="60"/>
         <source>Nuimo</source>
         <extracomment>The name of the DeviceClass (315ece51-053e-49f9-831f-b09f9f27fb9f)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Nuimo</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="63"/>
         <source>name</source>
         <extracomment>The name of the paramType (db67d1e6-26fa-44ed-ad55-c6aef45ea2ea) of nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>nombre</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="66"/>
         <source>mac address</source>
         <extracomment>The name of the paramType (71553f6a-2ed4-4896-bb7b-52e7dca948b2) of nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>dirección mac</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="75"/>
         <source>Hardware revision changed</source>
         <extracomment>The name of the autocreated EventType (5c400da4-a14e-4e0a-be9f-c82ffe7e1972)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de hardware modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="93"/>
         <source>battery changed</source>
         <extracomment>The name of the autocreated EventType (b5ee2465-7fa1-450b-8073-f115537d3409)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>batería modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="96"/>
         <source>battery</source>
         <extracomment>The name of the ParamType of the autocreated EventType (b5ee2465-7fa1-450b-8073-f115537d3409) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>batería</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="99"/>
         <source>rotation changed</source>
         <extracomment>The name of the autocreated EventType (69a5f495-5452-434b-8fb8-b73d992c5446)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>rotación modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="102"/>
         <source>rotation</source>
         <extracomment>The name of the ParamType of the autocreated EventType (69a5f495-5452-434b-8fb8-b73d992c5446) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>rotación</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="69"/>
         <source>Connected changed</source>
         <extracomment>The name of the autocreated EventType (5286976a-f5dc-4662-872a-438ac5d491cb)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Conexión modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="72"/>
         <source>Connected</source>
         <extracomment>The name of the ParamType of the autocreated EventType (5286976a-f5dc-4662-872a-438ac5d491cb) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Conectado</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="78"/>
         <source>Hardware revision</source>
         <extracomment>The name of the ParamType of the autocreated EventType (5c400da4-a14e-4e0a-be9f-c82ffe7e1972) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de hardware</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="81"/>
         <source>Firmware revision changed</source>
         <extracomment>The name of the autocreated EventType (edcf76c6-9fed-4c26-9853-c284cf887adb)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de firmware modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="84"/>
         <source>Firmware revision</source>
         <extracomment>The name of the ParamType of the autocreated EventType (edcf76c6-9fed-4c26-9853-c284cf887adb) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de firmware</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="87"/>
         <source>Software revision changed</source>
         <extracomment>The name of the autocreated EventType (be42cbd3-12e9-44ec-8f9d-141e10d9573a)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de software modificada</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="90"/>
         <source>Software revision</source>
         <extracomment>The name of the ParamType of the autocreated EventType (be42cbd3-12e9-44ec-8f9d-141e10d9573a) of DeviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Revisión de software</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="105"/>
         <source>Show logo</source>
         <extracomment>The name of the ActionType d44ca5b7-f8d6-4413-9d2e-cef89282c039 of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Mostrar logotipo</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="108"/>
         <source>logo</source>
         <extracomment>The name of the paramType (a8f72c37-9cb5-4885-a7e4-b2b396f8e4cd) of nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>logotipo</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="111"/>
         <source>clicked</source>
         <extracomment>The name of the EventType 5b9e08e8-7a6c-4311-9db6-82547847708c of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>pulsado</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="114"/>
         <source>Swipe left</source>
         <extracomment>The name of the EventType 2be36aa0-e2fe-4192-81c5-cf0bb7f08dd4 of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Arrastrar a izquierda</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="117"/>
         <source>Swipe right</source>
         <extracomment>The name of the EventType 81fb61ab-6d3d-4c1b-85fe-3dbd5c8dead7 of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Arrastrar a derecha</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="120"/>
         <source>Swipe up</source>
         <extracomment>The name of the EventType ab5c575b-b265-491c-93ca-ad9212374bc1 of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Arrastrar hacia arriba</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="123"/>
         <source>Swipe down</source>
         <extracomment>The name of the EventType 4d97cf28-1030-4a30-bed5-411102dd4b9b of deviceClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Arrastrar hacia abajo</translation>
     </message>
 </context>
 </TS>
