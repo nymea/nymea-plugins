343193db537f29b9c546112cad981877ab43353a
Bernhard Trinnes <bernhard.trinnes@guh.io>
2018-06-19 13:16:25 +0000

    Translated using Weblate (German)
    Currently translated at 97.0% (33 of 34 strings)
    
    Translation: nymea-plugins/kodi
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/kodi/de/

--- kodi/translations/e7186890-99fa-4c5b-8247-09c6d450d490-de_DE.ts
+++ kodi/translations/e7186890-99fa-4c5b-8247-09c6d450d490-de_DE.ts
@@ -19,31 +19,31 @@ The name of the DeviceClass (d09953e3-c5bd-415b-973b-0d0bf2be3f69)</extracomment
         <location filename="../plugininfo.h" line="78"/>
         <source>name</source>
         <extracomment>The name of the paramType (a704beb1-b0b0-46fc-91fc-3aac01e1a364) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Name</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="81"/>
         <source>ip</source>
         <extracomment>The name of the paramType (1a897065-57c6-49b3-bac9-1e5db27859e5) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>IP Adresse</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="84"/>
         <source>port</source>
         <extracomment>The name of the paramType (660fb4d7-9479-4c9d-a900-ce221d2b8ae4) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="87"/>
         <source>connected changed</source>
         <extracomment>The name of the autocreated EventType (09dfbd40-c97c-4a20-9ecd-f80e389a4864)</extracomment>
-        <translation>Verbunden geändert</translation>
+        <translation>Verbindung geändert</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="90"/>
         <source>connected</source>
         <extracomment>The name of the ParamType of the autocreated EventType (09dfbd40-c97c-4a20-9ecd-f80e389a4864) of DeviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="93"/>
@@ -58,13 +58,13 @@ The name of the DeviceClass (d09953e3-c5bd-415b-973b-0d0bf2be3f69)</extracomment
         <extracomment>The name of the ParamType of the autocreated EventType (bc98cdb0-4d0e-48ca-afc7-922e49bb7813) of DeviceClass kodi
 ----------
 The name of the autocreated ParamType of the writable StateType (bc98cdb0-4d0e-48ca-afc7-922e49bb7813) of DeviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Stumm</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="99"/>
         <source>Set mute</source>
         <extracomment>The name of the autocreated ActionType (bc98cdb0-4d0e-48ca-afc7-922e49bb7813)</extracomment>
-        <translation>Setze Stumm</translation>
+        <translation>Stumm schalten</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="105"/>
@@ -79,19 +79,19 @@ The name of the autocreated ParamType of the writable StateType (bc98cdb0-4d0e-4
         <extracomment>The name of the ParamType of the autocreated EventType (9dfe5d78-4c3f-497c-bab1-bb9fdf7e93a9) of DeviceClass kodi
 ----------
 The name of the autocreated ParamType of the writable StateType (9dfe5d78-4c3f-497c-bab1-bb9fdf7e93a9) of DeviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Lautstärke</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="111"/>
         <source>Set volume</source>
         <extracomment>The name of the autocreated ActionType (9dfe5d78-4c3f-497c-bab1-bb9fdf7e93a9)</extracomment>
-        <translation>Setze Lautstärke</translation>
+        <translation>Lautstärke einstellen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="117"/>
         <source>playback status changed</source>
         <extracomment>The name of the autocreated EventType (2dd512b7-40c2-488e-8d4f-6519edaa6f74)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Playbackstatus geändert</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="120"/>
@@ -100,91 +100,91 @@ The name of the autocreated ParamType of the writable StateType (9dfe5d78-4c3f-4
         <extracomment>The name of the ParamType of the autocreated EventType (2dd512b7-40c2-488e-8d4f-6519edaa6f74) of DeviceClass kodi
 ----------
 The name of the autocreated ParamType of the writable StateType (2dd512b7-40c2-488e-8d4f-6519edaa6f74) of DeviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Playbackstatus</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="123"/>
         <source>set playback status</source>
         <extracomment>The name of the autocreated ActionType (2dd512b7-40c2-488e-8d4f-6519edaa6f74)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Setze Playbackstatus</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="129"/>
         <source>skip back</source>
         <extracomment>The name of the ActionType a180807d-1265-4831-9d86-a421767418dd of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zurückspringen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="132"/>
         <source>rewind</source>
         <extracomment>The name of the ActionType 7e70b47b-7e79-4521-be34-04a3c427e5b1 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zurückspulen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="135"/>
         <source>stop</source>
         <extracomment>The name of the ActionType ae3cbe03-ee3e-410e-abbd-efabc2402198 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Stop</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="138"/>
         <source>play</source>
         <extracomment>The name of the ActionType 4d2ee668-a2e3-4795-8b96-0c800b703b46 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Abspielen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="141"/>
         <source>pause</source>
         <extracomment>The name of the ActionType 3cf341cb-fe63-40bc-a450-9678d18e91e3 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Pause</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="144"/>
         <source>fastForward</source>
         <extracomment>The name of the ActionType 6985250e-1188-4e44-b9dd-93303b5738a1 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Vorlauf</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="147"/>
         <source>skipNext</source>
         <extracomment>The name of the ActionType 85d7126a-b123-4a28-aeb4-d84bcfb4d14f of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Überspringen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="150"/>
         <source>show notification</source>
         <extracomment>The name of the ActionType dc0aa3b5-4eae-4e58-a4ac-d4c124da53f1 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Zeige Benachrichtigung</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="153"/>
         <source>message</source>
         <extracomment>The name of the paramType (798f720a-cc4f-40e7-91d7-2ef5957ca7ad) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Nachricht</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="156"/>
         <source>type</source>
         <extracomment>The name of the paramType (86df6a45-ee8a-4e1f-94e2-4081e14eb557) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Typ</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="159"/>
         <source>press button</source>
         <extracomment>The name of the ActionType 28060803-aa85-44a4-9dec-ee669dfb629f of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Dücke Taste</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="162"/>
         <source>button</source>
         <extracomment>The name of the paramType (93861dac-0c24-4a3b-903d-d1be44eae611) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Taste</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="165"/>
         <source>system</source>
         <extracomment>The name of the ActionType 8d51cd64-ecef-44f8-85b0-697bef252ce5 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>System</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="168"/>
@@ -196,37 +196,37 @@ The name of the autocreated ParamType of the writable StateType (2dd512b7-40c2-4
 The name of the paramType (3219855b-e043-43aa-91ae-794b474379bf) of kodi
 ----------
 The name of the paramType (ba89d098-56d7-40a2-b499-c02499c1ec0c) of kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Kommando</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="171"/>
         <source>video library</source>
         <extracomment>The name of the ActionType 59f7ad10-16eb-40b7-a88b-c8393ae8e413 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Videobibliothek</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="177"/>
         <source>audio library</source>
         <extracomment>The name of the ActionType 3d5120eb-1007-46c3-b76e-d9d4b105b9f2 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Audio-Bibliothek</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="183"/>
         <source>player play</source>
         <extracomment>The name of the EventType 2535a1eb-7643-4874-98f6-b027fdff6311 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Player abspielen</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="186"/>
         <source>player pause</source>
         <extracomment>The name of the EventType 99498b1c-e9c0-480a-9e91-662ee79ba976 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Player pause</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="189"/>
         <source>player stop</source>
         <extracomment>The name of the EventType a02ce255-3abb-435d-a92e-7f99c952ecb2 of deviceClass kodi</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Player stop</translation>
     </message>
 </context>
 </TS>
