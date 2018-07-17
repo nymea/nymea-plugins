b49d7f52c9db2e25d517eb5f08f9a2d447e6781d
Jenkins <jenkins@nymea.io>
2018-07-17 23:24:22 +0200

    Automatic translations update by Jenkins

--- flowercare/translations/74e2106a-3407-4e89-a27a-1c890d78bee7-en_US.ts
+++ flowercare/translations/74e2106a-3407-4e89-a27a-1c890d78bee7-en_US.ts
@@ -1,4 +1,99 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
 <TS version="2.1">
+<context>
+    <name>flowerCare</name>
+    <message>
+        <source>Flower Care</source>
+        <extracomment>The name of the plugin flowerCare (74e2106a-3407-4e89-a27a-1c890d78bee7)
+----------
+The name of the DeviceClass (297e2efa-168c-4eb8-bf3f-f8694dc0a2b9)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Xiaomi</source>
+        <extracomment>The name of the vendor (f037aa1a-f764-42f9-a613-338e683e4da5)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Name</source>
+        <extracomment>The name of the paramType (65f95e3c-59fe-47d6-ba4a-2469e5f554e2) of flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>MAC address</source>
+        <extracomment>The name of the paramType (fe2dba0f-2a47-463e-a645-bcd55cf09750) of flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reachable changed</source>
+        <extracomment>The name of the autocreated EventType (6254103a-ff8b-4f62-afda-6b5be1c933ec)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reachable</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (6254103a-ff8b-4f62-afda-6b5be1c933ec) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery level changed</source>
+        <extracomment>The name of the autocreated EventType (801a00ab-ce3b-4b10-9236-2f845c469a26)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery level</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (801a00ab-ce3b-4b10-9236-2f845c469a26) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery level critical changed</source>
+        <extracomment>The name of the autocreated EventType (700a83e6-9fbb-4330-b00e-e9d0874b4f66)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Battery level critical</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (700a83e6-9fbb-4330-b00e-e9d0874b4f66) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperature changed</source>
+        <extracomment>The name of the autocreated EventType (4577a032-bafd-49ca-8625-6dcf90819e88)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Temperature</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (4577a032-bafd-49ca-8625-6dcf90819e88) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Soil moisture changed</source>
+        <extracomment>The name of the autocreated EventType (5e3ac82a-1dda-4399-888e-529b391a814a)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Soil moisture</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (5e3ac82a-1dda-4399-888e-529b391a814a) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Light intensity changed</source>
+        <extracomment>The name of the autocreated EventType (7e0a3b3d-72f4-4547-bd1e-2711be61c1a7)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Light intensity</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (7e0a3b3d-72f4-4547-bd1e-2711be61c1a7) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conductivity changed</source>
+        <extracomment>The name of the autocreated EventType (f77608b3-8b5b-43a9-aab6-0dc35c694ec3)</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Conductivity</source>
+        <extracomment>The name of the ParamType of the autocreated EventType (f77608b3-8b5b-43a9-aab6-0dc35c694ec3) of DeviceClass flowerCare</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
 </TS>
