95a308083826d27f9e07241f2b66edfd4134a3c3
Christian Warejko <c.warejko@marantec.de>
2019-06-13 14:27:28 +0000

    Translated using Weblate (Danish)
    Currently translated at 100.0% (57 of 57 strings)
    
    Translation: nymea-plugins/datetime
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/datetime/da/

--- datetime/translations/c26014c6-87fb-4233-85ed-01d18625018d-da.ts
+++ datetime/translations/c26014c6-87fb-4233-85ed-01d18625018d-da.ts
@@ -109,7 +109,7 @@
         <location filename="../plugininfo.h" line="105"/>
         <source>nymea</source>
         <extracomment>The name of the vendor (2062d64d-3232-433c-88bc-0d33c0ba2ba6)</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>nymea</translation>
     </message>
     <message>
         <location filename="../plugininfo.h" line="114"/>
