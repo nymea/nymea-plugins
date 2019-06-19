088cf788886bb254c0bd4b88e8471f2e5efb8c3e
Simon Stürz <simon.stuerz@guh.io>
2019-06-19 11:41:53 +0200

    Migrate tasmota plugin documentation

--- tasmota/deviceplugintasmota.cpp
+++ tasmota/deviceplugintasmota.cpp
@@ -20,40 +20,6 @@
  *                                                                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-/*!
-    \page tasmota.html
-    \title Tasmota (Sonoff) devices
-    \brief Plugin for Tasmota devices (Sonoff devices flashed with Tasmota and others)
-
-    \ingroup plugins
-    \ingroup nymea-plugins-maker
-
-    This plugin allows to make use of Sonoff-Tasmota devices via the nymea internal MQTT broker.
-    There is no external MQTT broker needed.
-
-    Note that Sonoff devices must be flashed with the Tasmota sofware and connected to the WiFi network in order to
-    work with this plugin.
-
-    See https://github.com/arendst/Sonoff-Tasmota/wiki for a list of all supported devices and instructions on how to
-    install Tasmota on those.
-
-    After flashing Tasmota to a Sonoff device and connecting it to WiFi, it can be added to nymea. The only required
-    thing is the IP address to the device. This plugin will create a new isoloated MQTT channel on the nymea internal
-    MQTT broker and provision login details to the Tasmota device via HTTP. Once that is successful, the Tasmota device
-    will connect to the MQTT broker and appear as connected in nymea.
-
-    \chapter Plugin properties
-    When adding a Tasmota device it will add a new Gateway type device representing the Tasmota device itself. In addition
-    to that a power switch device will appear which can be used to control the switches in the Tasmota device. Upon
-    device setup, the user can optionally select the type of the connected hardware, (e.g. a light) which causes this
-    plugin to create a light device in the system which also controls the switches inside the Tasmota device and nicely
-    integrates with the nymea:ux for the given device type.
-
-    For more details how to read this JSON file please check out the documentation for \l{The plugin JSON File}.
-
-    \quotefile plugins/deviceplugins/tasmota/deviceplugintasmota.json
-*/
-
 #include "deviceplugintasmota.h"
 #include "plugininfo.h"
 
