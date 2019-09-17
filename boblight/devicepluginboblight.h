58476b4592264c83ae29d868abfb001dcab18309
Michael Zanetti <michael_zanetti@gmx.net>
2019-09-17 17:37:00 +0200

    Update Boblight to new API

--- boblight/devicepluginboblight.h
+++ boblight/devicepluginboblight.h
@@ -39,13 +39,13 @@ public:
 
     void init() override;
 
-    Device::DeviceSetupStatus setupDevice(Device *device) override;
+    void setupDevice(DeviceSetupInfo *info) override;
     void postSetupDevice(Device *device) override;
     void deviceRemoved(Device *device) override;
 
     void startMonitoringAutoDevices() override;
 
-    Device::DeviceError executeAction(Device *device, const Action &action) override;
+    void executeAction(DeviceActionInfo *info) override;
 
 private slots:
     void onConnectionChanged();
