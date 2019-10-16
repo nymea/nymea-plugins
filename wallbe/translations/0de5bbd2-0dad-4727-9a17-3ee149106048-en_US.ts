acb0b737cc58175aaeedcb8da4f34b947fabe1ad
Boernsman <bernhard.trinnes@guh.io>
2019-10-16 15:42:21 +0200

    added translation file

---
+++ wallbe/translations/0de5bbd2-0dad-4727-9a17-3ee149106048-en_US.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Wallbe</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="44"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="47"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="50"/>
+        <source>Charging</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, ActionType: power, ID: {26793adc-de10-426f-bb17-170c227891b2})
+----------
+The name of the ParamType (DeviceClass: wallbeEco, EventType: power, ID: {26793adc-de10-426f-bb17-170c227891b2})
+----------
+The name of the StateType ({26793adc-de10-426f-bb17-170c227891b2}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="53"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="56"/>
+        <source>Charging Time</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, EventType: chargeTime, ID: {8dc2fef8-d16e-422a-8498-456b818f5752})
+----------
+The name of the StateType ({8dc2fef8-d16e-422a-8498-456b818f5752}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="59"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="62"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="65"/>
+        <source>Charging current</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, ActionType: chargeCurrent, ID: {60b5b6b8-bcd3-4c3f-8501-f15af94bc8c1})
+----------
+The name of the ParamType (DeviceClass: wallbeEco, EventType: chargeCurrent, ID: {60b5b6b8-bcd3-4c3f-8501-f15af94bc8c1})
+----------
+The name of the StateType ({60b5b6b8-bcd3-4c3f-8501-f15af94bc8c1}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="68"/>
+        <source>Charging current changed</source>
+        <extracomment>The name of the EventType ({60b5b6b8-bcd3-4c3f-8501-f15af94bc8c1}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="71"/>
+        <source>Charging status changed</source>
+        <extracomment>The name of the EventType ({26793adc-de10-426f-bb17-170c227891b2}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="74"/>
+        <source>Charging time changed</source>
+        <extracomment>The name of the EventType ({8dc2fef8-d16e-422a-8498-456b818f5752}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="77"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="80"/>
+        <source>Connected</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, EventType: connected, ID: {39a8e92b-40e5-4648-b5a8-2ffcb5598081})
+----------
+The name of the StateType ({39a8e92b-40e5-4648-b5a8-2ffcb5598081}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="83"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the EventType ({39a8e92b-40e5-4648-b5a8-2ffcb5598081}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="86"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="89"/>
+        <source>EV Status</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, EventType: evStatus, ID: {2a95c4fb-9a15-4788-ae09-d34e71314da6})
+----------
+The name of the StateType ({2a95c4fb-9a15-4788-ae09-d34e71314da6}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="92"/>
+        <source>IP address</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, Type: device, ID: {95f297a7-56a5-4789-9b14-6735717344b5})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="95"/>
+        <source>MAC address</source>
+        <extracomment>The name of the ParamType (DeviceClass: wallbeEco, Type: device, ID: {551b03f0-dd70-4463-929b-3668dbd3290f})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="98"/>
+        <source>Petring</source>
+        <extracomment>The name of the vendor ({831b4b87-0a6c-4d51-b055-967bb6e5fab5})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="101"/>
+        <source>Set charging current</source>
+        <extracomment>The name of the ActionType ({60b5b6b8-bcd3-4c3f-8501-f15af94bc8c1}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="104"/>
+        <source>Start charging</source>
+        <extracomment>The name of the ActionType ({26793adc-de10-426f-bb17-170c227891b2}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="107"/>
+        <source>Wallbe eco 2.0</source>
+        <extracomment>The name of the DeviceClass ({e66c84f6-b398-47e9-8aeb-33840e7b4492})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="110"/>
+        <source>ev status changed</source>
+        <extracomment>The name of the EventType ({2a95c4fb-9a15-4788-ae09-d34e71314da6}) of DeviceClass wallbeEco</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/wallbe/plugininfo.h" line="113"/>
+        <source>wallbe plugin</source>
+        <extracomment>The name of the plugin Wallbe ({0de5bbd2-0dad-4727-9a17-3ee149106048})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
