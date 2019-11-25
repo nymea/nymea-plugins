6abb4b8a530e828de3cc23d31bfbcc3c1b42d388
Boernsman <bernhard.trinnes@guh.io>
2019-11-25 21:58:38 +0100

    updated translation

--- i2ctools/translations/284d7f44-09aa-444e-afc3-5acee8f2ab43-en_US.ts
+++ i2ctools/translations/284d7f44-09aa-444e-afc3-5acee8f2ab43-en_US.ts
@@ -26,52 +26,10 @@ The name of the StateType ({6a1a5da4-a8ff-4173-8238-6196aecef545}) of DeviceClas
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="51"/>
-        <source>I2C Tools</source>
-        <extracomment>The name of the plugin I2cTools ({284d7f44-09aa-444e-afc3-5acee8f2ab43})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="54"/>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="57"/>
-        <source>I2C interface</source>
-        <extracomment>The name of the ParamType (DeviceClass: i2cInterface, Type: device, ID: {406b9787-62c1-4697-9c97-fe4105f365d8})
-----------
-The name of the DeviceClass ({468dbda3-4364-4255-8060-25e80ab9fc29})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="60"/>
-        <source>I2C read register</source>
-        <extracomment>The name of the DeviceClass ({f6c2ab46-3a70-4dbd-aae4-62fb7e984514})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="63"/>
-        <source>Register</source>
-        <extracomment>The name of the ParamType (DeviceClass: i2cReadRegister, Type: device, ID: {27a86394-d89f-4140-966d-ed8d5eb65f64})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="66"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="69"/>
-        <source>Value</source>
-        <extracomment>The name of the ParamType (DeviceClass: i2cReadRegister, EventType: value, ID: {72bf6436-e55e-45c7-b1c7-f459ae587b00})
-----------
-The name of the StateType ({72bf6436-e55e-45c7-b1c7-f459ae587b00}) of DeviceClass i2cReadRegister</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="72"/>
-        <source>Value changed</source>
-        <extracomment>The name of the EventType ({72bf6436-e55e-45c7-b1c7-f459ae587b00}) of DeviceClass i2cReadRegister</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="75"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="78"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="81"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="84"/>
-        <source>connected</source>
+        <source>Connected</source>
         <extracomment>The name of the ParamType (DeviceClass: i2cReadRegister, EventType: connected, ID: {726befc0-37cd-47f1-b983-1b5ee87a5bf7})
 ----------
 The name of the StateType ({726befc0-37cd-47f1-b983-1b5ee87a5bf7}) of DeviceClass i2cReadRegister
@@ -82,18 +40,60 @@ The name of the StateType ({a108bdaf-2067-4ec9-bd45-89c5eecb8038}) of DeviceClas
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="87"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="90"/>
-        <source>connected changed</source>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="63"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="66"/>
+        <source>Connected changed</source>
         <extracomment>The name of the EventType ({726befc0-37cd-47f1-b983-1b5ee87a5bf7}) of DeviceClass i2cReadRegister
 ----------
 The name of the EventType ({a108bdaf-2067-4ec9-bd45-89c5eecb8038}) of DeviceClass i2cInterface</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="69"/>
+        <source>I2C Tools</source>
+        <extracomment>The name of the plugin I2cTools ({284d7f44-09aa-444e-afc3-5acee8f2ab43})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="72"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="75"/>
+        <source>I2C interface</source>
+        <extracomment>The name of the ParamType (DeviceClass: i2cInterface, Type: device, ID: {406b9787-62c1-4697-9c97-fe4105f365d8})
+----------
+The name of the DeviceClass ({468dbda3-4364-4255-8060-25e80ab9fc29})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="78"/>
+        <source>I2C read register</source>
+        <extracomment>The name of the DeviceClass ({f6c2ab46-3a70-4dbd-aae4-62fb7e984514})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="81"/>
+        <source>Register</source>
+        <extracomment>The name of the ParamType (DeviceClass: i2cReadRegister, Type: device, ID: {27a86394-d89f-4140-966d-ed8d5eb65f64})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="84"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="87"/>
+        <source>Value</source>
+        <extracomment>The name of the ParamType (DeviceClass: i2cReadRegister, EventType: value, ID: {72bf6436-e55e-45c7-b1c7-f459ae587b00})
+----------
+The name of the StateType ({72bf6436-e55e-45c7-b1c7-f459ae587b00}) of DeviceClass i2cReadRegister</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="90"/>
+        <source>Value changed</source>
+        <extracomment>The name of the EventType ({72bf6436-e55e-45c7-b1c7-f459ae587b00}) of DeviceClass i2cReadRegister</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/i2ctools/plugininfo.h" line="93"/>
         <source>nymea</source>
-        <extracomment>The name of the vendor ({b9167232-2092-4864-9a1c-5a99d9882ca8})</extracomment>
+        <extracomment>The name of the vendor ({2062d64d-3232-433c-88bc-0d33c0ba2ba6})</extracomment>
         <translation type="unfinished"></translation>
     </message>
 </context>
