66fa7c295e5fffe2354717d270fc74f08542c64d
Boernsman <bernhard.trinnes@guh.io>
2019-12-23 15:10:29 +0100

    added translations

---
+++ aqi/translations/57d69b76-4d2d-41ec-bef6-949a79ffbe6b-en_US.ts
@@ -0,0 +1,234 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>AirQualityIndex</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="63"/>
+        <source>API key</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, Type: plugin, ID: {a3525f8a-18be-4739-b0ea-ef3af0c7f280})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="66"/>
+        <source>Air Quality Index</source>
+        <extracomment>The name of the vendor ({6c8e2ded-0a33-4e77-b76c-ea02168741ec})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="69"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="72"/>
+        <source>Air quality</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: airQuality, ID: {33a3329a-4117-4488-aa18-91c76056ed6e})
+----------
+The name of the StateType ({33a3329a-4117-4488-aa18-91c76056ed6e}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="75"/>
+        <source>Air quality changed</source>
+        <extracomment>The name of the EventType ({33a3329a-4117-4488-aa18-91c76056ed6e}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="78"/>
+        <source>Air quality index</source>
+        <extracomment>The name of the DeviceClass ({23ea32c9-38b0-4155-bacc-3afa8c09f6ee})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="81"/>
+        <source>AirQualityIndex</source>
+        <extracomment>The name of the plugin AirQualityIndex ({57d69b76-4d2d-41ec-bef6-949a79ffbe6b})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="84"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="87"/>
+        <source>Carbon monoxide level (CO)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: co, ID: {54ac72f3-6444-46a8-a43d-210c2a6fbfb5})
+----------
+The name of the StateType ({54ac72f3-6444-46a8-a43d-210c2a6fbfb5}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="90"/>
+        <source>Carbon monoxide level (CO) changed</source>
+        <extracomment>The name of the EventType ({54ac72f3-6444-46a8-a43d-210c2a6fbfb5}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="93"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="96"/>
+        <source>Cautionary statement</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: cautionaryStatement, ID: {cfece671-4e88-4c49-9456-e3f8f7c79ab3})
+----------
+The name of the StateType ({cfece671-4e88-4c49-9456-e3f8f7c79ab3}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="99"/>
+        <source>Cautionary statement changed</source>
+        <extracomment>The name of the EventType ({cfece671-4e88-4c49-9456-e3f8f7c79ab3}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="102"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="105"/>
+        <source>Coarse dust particles pollution level (PM10)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: pm10, ID: {24b41ec4-e26b-4dfb-b52c-8e2b1bbdafc6})
+----------
+The name of the StateType ({24b41ec4-e26b-4dfb-b52c-8e2b1bbdafc6}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="108"/>
+        <source>Coarse dust particles pollution level (PM10) changed</source>
+        <extracomment>The name of the EventType ({24b41ec4-e26b-4dfb-b52c-8e2b1bbdafc6}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="111"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="114"/>
+        <source>Connected</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: connected, ID: {7b9135cd-2461-4d33-b2b3-3dc600983895})
+----------
+The name of the StateType ({7b9135cd-2461-4d33-b2b3-3dc600983895}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="117"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the EventType ({7b9135cd-2461-4d33-b2b3-3dc600983895}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="120"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="123"/>
+        <source>Fine particles pollution level (PM2.5)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: pm25, ID: {bc8c4c83-d229-4be4-8732-bc4f2390f399})
+----------
+The name of the StateType ({bc8c4c83-d229-4be4-8732-bc4f2390f399}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="126"/>
+        <source>Fine particles pollution level (PM2.5) changed</source>
+        <extracomment>The name of the EventType ({bc8c4c83-d229-4be4-8732-bc4f2390f399}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="129"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="132"/>
+        <source>Humidity</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: humidity, ID: {4fc45fca-25ab-45a0-b862-817eea1f51e3})
+----------
+The name of the StateType ({4fc45fca-25ab-45a0-b862-817eea1f51e3}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="135"/>
+        <source>Humidity changed</source>
+        <extracomment>The name of the EventType ({4fc45fca-25ab-45a0-b862-817eea1f51e3}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="138"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="141"/>
+        <source>Nitrogen Dioxide level (NO2)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: no2, ID: {6ed6c505-f36e-44c4-a982-f395b04e539b})
+----------
+The name of the StateType ({6ed6c505-f36e-44c4-a982-f395b04e539b}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="144"/>
+        <source>Nitrogen Dioxide level (NO2) changed</source>
+        <extracomment>The name of the EventType ({6ed6c505-f36e-44c4-a982-f395b04e539b}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="147"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="150"/>
+        <source>Ozone level (O3)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: o3, ID: {4e88526d-009f-4820-9a84-09b3646d23c9})
+----------
+The name of the StateType ({4e88526d-009f-4820-9a84-09b3646d23c9}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="153"/>
+        <source>Ozone level (O3) changed</source>
+        <extracomment>The name of the EventType ({4e88526d-009f-4820-9a84-09b3646d23c9}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="156"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="159"/>
+        <source>Pressure</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: pressure, ID: {5f799040-08f8-44d1-aa0a-4cab7caad839})
+----------
+The name of the StateType ({5f799040-08f8-44d1-aa0a-4cab7caad839}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="162"/>
+        <source>Pressure changed</source>
+        <extracomment>The name of the EventType ({5f799040-08f8-44d1-aa0a-4cab7caad839}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="165"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="168"/>
+        <source>Station name</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: stationName, ID: {8385f3d5-62f7-482e-927c-b5d61a70d607})
+----------
+The name of the StateType ({8385f3d5-62f7-482e-927c-b5d61a70d607}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="171"/>
+        <source>Station name changed</source>
+        <extracomment>The name of the EventType ({8385f3d5-62f7-482e-927c-b5d61a70d607}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="174"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="177"/>
+        <source>Sulfur dioxide level (SO2)</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: so2, ID: {f3a05e65-a9b3-48fd-be43-688d4c293cc9})
+----------
+The name of the StateType ({f3a05e65-a9b3-48fd-be43-688d4c293cc9}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="180"/>
+        <source>Sulfur dioxide level (SO2) changed</source>
+        <extracomment>The name of the EventType ({f3a05e65-a9b3-48fd-be43-688d4c293cc9}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="183"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="186"/>
+        <source>Temperature</source>
+        <extracomment>The name of the ParamType (DeviceClass: airQualityIndex, EventType: temperature, ID: {94219802-0a82-4761-99b3-c6b6dfc096db})
+----------
+The name of the StateType ({94219802-0a82-4761-99b3-c6b6dfc096db}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/aqi/plugininfo.h" line="189"/>
+        <source>Temperature changed</source>
+        <extracomment>The name of the EventType ({94219802-0a82-4761-99b3-c6b6dfc096db}) of DeviceClass airQualityIndex</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>DevicePluginAqi</name>
+    <message>
+        <location filename="../devicepluginaqi.cpp" line="43"/>
+        <source>Service is already in use</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
