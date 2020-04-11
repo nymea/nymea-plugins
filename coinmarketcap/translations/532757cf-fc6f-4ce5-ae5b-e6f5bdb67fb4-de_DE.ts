1321934af27d188e930ba73e0686592ad5b73590
bernhard.trinnes <bernhard.trinnes@nymea.io>
2020-04-12 00:51:56 +0700

    added german translation

--- coinmarketcap/translations/532757cf-fc6f-4ce5-ae5b-e6f5bdb67fb4-de_DE.ts
+++ coinmarketcap/translations/532757cf-fc6f-4ce5-ae5b-e6f5bdb67fb4-de_DE.ts
@@ -1,23 +1,23 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="de_DE">
 <context>
     <name>IntegrationPluginCoinMarketCap</name>
     <message>
         <location filename="../integrationplugincoinmarketcap.cpp" line="50"/>
         <source>CoinMarketCap server is not reachable.</source>
-        <translation type="unfinished"></translation>
+        <translation>Der CoinMarketCap-Server ist nicht erreichbar.</translation>
     </message>
     <message>
         <location filename="../integrationplugincoinmarketcap.cpp" line="52"/>
         <source>Please enter your API token.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bitte geben Sie Ihr API-Token ein.</translation>
     </message>
     <message>
         <location filename="../integrationplugincoinmarketcap.cpp" line="74"/>
         <source>This token is not valid.</source>
         <extracomment>Error setting up device with invalid token</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Dieser Token ist ungültig.</translation>
     </message>
 </context>
 <context>
@@ -29,7 +29,7 @@
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: btc, ID: {721c91c8-8eb1-402e-8bfc-8e2d496be032})
 ----------
 The name of the StateType ({721c91c8-8eb1-402e-8bfc-8e2d496be032}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="63"/>
@@ -38,19 +38,19 @@ The name of the StateType ({721c91c8-8eb1-402e-8bfc-8e2d496be032}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: bch, ID: {cc5b4fe5-aa43-43b3-952f-90c0e3e20740})
 ----------
 The name of the StateType ({cc5b4fe5-aa43-43b3-952f-90c0e3e20740}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="69"/>
         <source>Bitcoin Cash value changed</source>
         <extracomment>The name of the EventType ({cc5b4fe5-aa43-43b3-952f-90c0e3e20740}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bitcoin Cash Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="72"/>
         <source>Bitcoin value changed</source>
         <extracomment>The name of the EventType ({721c91c8-8eb1-402e-8bfc-8e2d496be032}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Bitcoin Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="75"/>
@@ -59,7 +59,7 @@ The name of the StateType ({cc5b4fe5-aa43-43b3-952f-90c0e3e20740}) of ThingClass
         <extracomment>The name of the vendor ({28eeecbd-4178-45aa-81a6-fd95851444fd})
 ----------
 The name of the plugin coinMarketCap ({532757cf-fc6f-4ce5-ae5b-e6f5bdb67fb4})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>CoinMarketCap</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="81"/>
@@ -68,19 +68,19 @@ The name of the plugin coinMarketCap ({532757cf-fc6f-4ce5-ae5b-e6f5bdb67fb4})</e
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: connected, ID: {19f079f0-1654-44c3-ab10-e7d7f9742e09})
 ----------
 The name of the StateType ({19f079f0-1654-44c3-ab10-e7d7f9742e09}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbunden</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="87"/>
         <source>Connection status changed</source>
         <extracomment>The name of the EventType ({19f079f0-1654-44c3-ab10-e7d7f9742e09}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="90"/>
         <source>Crypto Prices</source>
         <extracomment>The name of the ThingClass ({f5b794f7-7e2a-4409-8445-23ead80f2a8f})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Verbunden geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="93"/>
@@ -89,13 +89,13 @@ The name of the StateType ({19f079f0-1654-44c3-ab10-e7d7f9742e09}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: dash, ID: {0b602f14-1596-4091-a0ab-6c5efa087a3f})
 ----------
 The name of the StateType ({0b602f14-1596-4091-a0ab-6c5efa087a3f}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="99"/>
         <source>Dash value changed</source>
         <extracomment>The name of the EventType ({0b602f14-1596-4091-a0ab-6c5efa087a3f}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Dash Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="102"/>
@@ -104,7 +104,7 @@ The name of the StateType ({0b602f14-1596-4091-a0ab-6c5efa087a3f}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: eth, ID: {5c58bb9f-dcdf-47c9-a0b7-f0ffc968f824})
 ----------
 The name of the StateType ({5c58bb9f-dcdf-47c9-a0b7-f0ffc968f824}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="108"/>
@@ -113,25 +113,25 @@ The name of the StateType ({5c58bb9f-dcdf-47c9-a0b7-f0ffc968f824}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: etc, ID: {fc9a61b7-c3df-4f13-ba31-d7743f3d2dc2})
 ----------
 The name of the StateType ({fc9a61b7-c3df-4f13-ba31-d7743f3d2dc2}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="114"/>
         <source>Ethereum value changed</source>
         <extracomment>The name of the EventType ({5c58bb9f-dcdf-47c9-a0b7-f0ffc968f824}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Etherium Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="117"/>
         <source>Etherium Classic value changed</source>
         <extracomment>The name of the EventType ({fc9a61b7-c3df-4f13-ba31-d7743f3d2dc2}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Etherium Classic Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="120"/>
         <source>Fiat Currency</source>
         <extracomment>The name of the ParamType (ThingClass: currentPrices, Type: thing, ID: {92747d75-d18a-4915-bd48-0edd5cc5f19a})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="123"/>
@@ -140,13 +140,13 @@ The name of the StateType ({fc9a61b7-c3df-4f13-ba31-d7743f3d2dc2}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: miota, ID: {2c6e341f-5940-40bb-b495-89c3ecaaf9e7})
 ----------
 The name of the StateType ({2c6e341f-5940-40bb-b495-89c3ecaaf9e7}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="129"/>
         <source>IOTA value changed</source>
         <extracomment>The name of the EventType ({2c6e341f-5940-40bb-b495-89c3ecaaf9e7}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>IOTA Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="132"/>
@@ -155,13 +155,13 @@ The name of the StateType ({2c6e341f-5940-40bb-b495-89c3ecaaf9e7}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: ltc, ID: {f591f699-ae68-4f64-a221-254f836a31d3})
 ----------
 The name of the StateType ({f591f699-ae68-4f64-a221-254f836a31d3}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="138"/>
         <source>Litecoin value changed</source>
         <extracomment>The name of the EventType ({f591f699-ae68-4f64-a221-254f836a31d3}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Litecoin Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="141"/>
@@ -170,13 +170,13 @@ The name of the StateType ({f591f699-ae68-4f64-a221-254f836a31d3}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: xem, ID: {adecda44-fade-4dba-8b41-cfb07579dea9})
 ----------
 The name of the StateType ({adecda44-fade-4dba-8b41-cfb07579dea9}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="147"/>
         <source>NEM value changed</source>
         <extracomment>The name of the EventType ({adecda44-fade-4dba-8b41-cfb07579dea9}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>NEM Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="150"/>
@@ -185,13 +185,13 @@ The name of the StateType ({adecda44-fade-4dba-8b41-cfb07579dea9}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: ans, ID: {7583c720-1786-4c32-9552-e731578ff113})
 ----------
 The name of the StateType ({7583c720-1786-4c32-9552-e731578ff113}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="156"/>
         <source>NEO value changed</source>
         <extracomment>The name of the EventType ({7583c720-1786-4c32-9552-e731578ff113}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>NEO Wert geändert</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="159"/>
@@ -200,13 +200,13 @@ The name of the StateType ({7583c720-1786-4c32-9552-e731578ff113}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: currentPrices, EventType: xrp, ID: {d8517cf8-2ebd-4be3-8573-00b595cc2ab5})
 ----------
 The name of the StateType ({d8517cf8-2ebd-4be3-8573-00b595cc2ab5}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/coinmarketcap/plugininfo.h" line="165"/>
         <source>XRP value changed</source>
         <extracomment>The name of the EventType ({d8517cf8-2ebd-4be3-8573-00b595cc2ab5}) of ThingClass currentPrices</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>XRP Wert geändert</translation>
     </message>
 </context>
 </TS>
