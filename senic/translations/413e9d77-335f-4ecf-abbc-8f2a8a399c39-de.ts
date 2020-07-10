b428b237bfc17398fa0515b372734e20fb3c89a9
Jannik Höke <j.hoeke@marantec.de>
2020-07-10 11:32:21 +0000

    Translated using Weblate (German)
    Currently translated at 57.1% (16 of 28 strings)
    
    Translation: nymea-plugins/senic
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/senic/de/

--- senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-de.ts
+++ senic/translations/413e9d77-335f-4ecf-abbc-8f2a8a399c39-de.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../integrationpluginsenic.cpp" line="52"/>
         <source>Bluetooth is not available on this system.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth ist nicht verfügbar für dieses System.</translation>
     </message>
     <message>
         <location filename="../integrationpluginsenic.cpp" line="55"/>
         <source>Bluetooth is disabled. Please enable Bluetooth and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Bluetooth deaktiviert. Bitte aktivieren Sie Bluetooth und versuchen Sie es erneut.</translation>
     </message>
     <message>
         <location filename="../integrationpluginsenic.cpp" line="64"/>
         <source>An error happened during Bluetooth discovery.</source>
-        <translation type="unfinished"></translation>
+        <translation>Ein Fehler ist während der Bluetooth Discovery passiert.</translation>
     </message>
     <message>
         <location filename="../integrationpluginsenic.cpp" line="124"/>
         <source>Error connecting to nuimo.</source>
-        <translation type="unfinished"></translation>
+        <translation>Fehler beim Verbinden zum Nuimo.</translation>
     </message>
 </context>
 <context>
@@ -51,7 +51,7 @@ The name of the plugin Senic ({413e9d77-335f-4ecf-abbc-8f2a8a399c39})</extracomm
         <location filename="../../../build-nymea-plugins-Desktop-Debug/senic/plugininfo.h" line="79"/>
         <source>Button pressed</source>
         <extracomment>The name of the EventType ({24649eb2-47d1-4a2b-8c09-1f074382e2c4}) of ThingClass nuimo</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Button gedrückt.</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/senic/plugininfo.h" line="76"/>
