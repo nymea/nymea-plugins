89e3ed4d442a805cf59b3ff60c8048cf3d1bc719
Jannik Höke <j.hoeke@marantec.de>
2020-07-16 09:32:36 +0000

    Translated using Weblate (Italian)
    Currently translated at 77.4% (72 of 93 strings)
    
    Translation: nymea-plugins/philipshue
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/philipshue/it/

--- philipshue/translations/5f2e634b-b7f3-48ee-976a-b5ae22aa5c55-it.ts
+++ philipshue/translations/5f2e634b-b7f3-48ee-976a-b5ae22aa5c55-it.ts
@@ -490,7 +490,7 @@ The name of the ParamType (ThingClass: remote, EventType: pressed, ID: {e4e3eb3a
         <extracomment>The name of the ParamType (ThingClass: motionSensor, Type: thing, ID: {9cb488b7-a76f-4389-a6b5-b36250246f2b})
 ----------
 The name of the ParamType (ThingClass: outdoorSensor, Type: thing, ID: {3ca8632d-7bd1-45a9-86af-c856e006c334})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Model id</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/philipshue/plugininfo.h" line="530"/>
@@ -499,7 +499,7 @@ The name of the ParamType (ThingClass: outdoorSensor, Type: thing, ID: {3ca8632d
         <extracomment>The name of the ParamType (ThingClass: motionSensor, Type: thing, ID: {3e9aad4c-1dff-42c0-bbd4-cdd8635c01a7})
 ----------
 The name of the ParamType (ThingClass: outdoorSensor, Type: thing, ID: {4a15f861-cad6-464a-b250-08793c68ae30})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Uuid</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/philipshue/plugininfo.h" line="497"/>
