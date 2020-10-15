238a8bbd54b548fe4b11734353b57dec19ec9538
Jannik Höke <j.hoeke@marantec.de>
2020-10-15 11:01:22 +0000

    Translated using Weblate (English (United States))
    Currently translated at 86.6% (13 of 15 strings)
    
    Translation: nymea-plugins/tcpcommander
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/tcpcommander/en_US/

--- tcpcommander/translations/741b7b0a-0c9c-4c93-be99-0d0bcf5a4643-en_US.ts
+++ tcpcommander/translations/741b7b0a-0c9c-4c93-be99-0d0bcf5a4643-en_US.ts
@@ -1,17 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>IntegrationPluginTcpCommander</name>
     <message>
         <location filename="../integrationplugintcpcommander.cpp" line="56"/>
         <source>Error connecting to remote server.</source>
-        <translation type="unfinished"></translation>
+        <translation>Error connecting to remote server.</translation>
     </message>
     <message>
         <location filename="../integrationplugintcpcommander.cpp" line="77"/>
         <source>Error opening TCP port.</source>
-        <translation type="unfinished"></translation>
+        <translation>Error opening TCP port.</translation>
     </message>
 </context>
 <context>
@@ -20,13 +20,13 @@
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="91"/>
         <source>TCP Output</source>
         <extracomment>The name of the ThingClass ({c67d059f-694f-47cb-8e1d-9e3e6d014c1a})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>TCP output</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="76"/>
         <source>IPv4 Address</source>
         <extracomment>The name of the ParamType (ThingClass: tcpOutput, Type: thing, ID: {2a3fcb23-931b-4ba1-b134-c49b656c76f7})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>IPv4 address</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="40"/>
@@ -41,7 +41,7 @@ The name of the StateType ({a2eb1619-261c-45ee-9587-6b5994633ad0}) of ThingClass
 The name of the ParamType (ThingClass: tcpOutput, EventType: connected, ID: {725b541a-9e0c-4634-81eb-e415c0b8f025})
 ----------
 The name of the StateType ({725b541a-9e0c-4634-81eb-e415c0b8f025}) of ThingClass tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Connected</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="52"/>
@@ -50,13 +50,13 @@ The name of the StateType ({725b541a-9e0c-4634-81eb-e415c0b8f025}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: tcpInput, EventType: connectionCount, ID: {e959417f-55d4-4316-88f4-d3fa29976841})
 ----------
 The name of the StateType ({e959417f-55d4-4316-88f4-d3fa29976841}) of ThingClass tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Connection count</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="58"/>
         <source>Connection count changed</source>
         <extracomment>The name of the EventType ({e959417f-55d4-4316-88f4-d3fa29976841}) of ThingClass tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Connection count changed</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="61"/>
@@ -65,13 +65,13 @@ The name of the StateType ({e959417f-55d4-4316-88f4-d3fa29976841}) of ThingClass
         <extracomment>The name of the EventType ({a2eb1619-261c-45ee-9587-6b5994633ad0}) of ThingClass tcpInput
 ----------
 The name of the EventType ({725b541a-9e0c-4634-81eb-e415c0b8f025}) of ThingClass tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Connection status changed</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="73"/>
         <source>Data recieved</source>
         <extracomment>The name of the EventType ({6d7c6df6-cb61-4d9e-b0d7-37c43911ca4b}) of ThingClass tcpInput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Data received</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="79"/>
@@ -80,25 +80,25 @@ The name of the EventType ({725b541a-9e0c-4634-81eb-e415c0b8f025}) of ThingClass
         <extracomment>The name of the ParamType (ThingClass: tcpInput, Type: thing, ID: {88eb227d-13f7-451c-babf-1b141c219fd4})
 ----------
 The name of the ParamType (ThingClass: tcpOutput, Type: thing, ID: {bee8b151-815a-4159-9d8a-42b76e99b42c})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="94"/>
         <source>Tcp Commander</source>
         <extracomment>The name of the plugin TCPCommander ({741b7b0a-0c9c-4c93-be99-0d0bcf5a4643})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>TCP commander</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="97"/>
         <source>nymea</source>
         <extracomment>The name of the vendor ({2062d64d-3232-433c-88bc-0d33c0ba2ba6})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>nymea</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="85"/>
         <source>Send Data</source>
         <extracomment>The name of the ActionType ({6bc52462-b192-46a4-a6df-92cc5a479c89}) of ThingClass tcpOutput</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Send data.</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/tcpcommander/plugininfo.h" line="67"/>
