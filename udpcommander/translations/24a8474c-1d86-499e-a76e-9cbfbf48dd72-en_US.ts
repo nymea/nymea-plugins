df59d45e8319efbcb4d39511dd285085829ff959
Jannik Höke <j.hoeke@marantec.de>
2020-10-15 11:05:59 +0000

    Translated using Weblate (English (United States))
    Currently translated at 100.0% (9 of 9 strings)
    
    Translation: nymea-plugins/udpcommander
    Translate-URL: http://translate.nymea.io/projects/nymea-plugins/udpcommander/en_US/

--- udpcommander/translations/24a8474c-1d86-499e-a76e-9cbfbf48dd72-en_US.ts
+++ udpcommander/translations/24a8474c-1d86-499e-a76e-9cbfbf48dd72-en_US.ts
@@ -1,12 +1,12 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS>
-<TS version="2.1">
+<TS version="2.1" language="en_US">
 <context>
     <name>IntegrationPluginUdpCommander</name>
     <message>
         <location filename="../integrationpluginudpcommander.cpp" line="52"/>
         <source>Error opening UDP port.</source>
-        <translation type="unfinished"></translation>
+        <translation>Error opening UDP port.</translation>
     </message>
 </context>
 <context>
@@ -18,19 +18,19 @@
         <extracomment>The name of the ThingClass ({31b00639-8904-4522-84ed-54c46a54c63c})
 ----------
 The name of the plugin UdpCommander ({24a8474c-1d86-499e-a76e-9cbfbf48dd72})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>UDP commander</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="61"/>
         <source>nymea</source>
         <extracomment>The name of the vendor ({2062d64d-3232-433c-88bc-0d33c0ba2ba6})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>nymea</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="58"/>
         <source>UDP Receiver</source>
         <extracomment>The name of the ThingClass ({6ecd5a8d-595a-4520-85e3-dcc9679edf66})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>UDP receiver</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="43"/>
@@ -39,25 +39,25 @@ The name of the plugin UdpCommander ({24a8474c-1d86-499e-a76e-9cbfbf48dd72})</ex
         <extracomment>The name of the ParamType (ThingClass: udpCommander, Type: thing, ID: {eb204dfe-11f8-413f-8b68-8bffef3f9a7f})
 ----------
 The name of the ParamType (ThingClass: udpReceiver, Type: thing, ID: {1843adcb-e377-44d1-8d70-ab4f9eeb32ec})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Port</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="40"/>
         <source>Data received</source>
         <extracomment>The name of the EventType ({5fecbba3-ffbb-456b-872c-a2f571c681cb}) of ThingClass udpReceiver</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Data received</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="31"/>
         <source>Address</source>
         <extracomment>The name of the ParamType (ThingClass: udpCommander, Type: thing, ID: {ea1be9fc-9a9b-44ba-a28d-e021aef4f046})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Address</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="49"/>
         <source>Send data</source>
         <extracomment>The name of the ActionType ({6bc52462-b192-46a4-a6df-92cc5a479c89}) of ThingClass udpCommander</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Send data</translation>
     </message>
     <message>
         <location filename="../../../build-nymea-plugins-Desktop-Debug/udpcommander/plugininfo.h" line="34"/>
@@ -66,7 +66,7 @@ The name of the ParamType (ThingClass: udpReceiver, Type: thing, ID: {1843adcb-e
         <extracomment>The name of the ParamType (ThingClass: udpCommander, ActionType: trigger, ID: {6604c852-6b24-4707-b8e5-1ddd8032efcc})
 ----------
 The name of the ParamType (ThingClass: udpReceiver, EventType: triggered, ID: {93c379e3-4707-46b7-9be0-2d6ff88f5d21})</extracomment>
-        <translation type="unfinished"></translation>
+        <translation>Data</translation>
     </message>
 </context>
 </TS>
