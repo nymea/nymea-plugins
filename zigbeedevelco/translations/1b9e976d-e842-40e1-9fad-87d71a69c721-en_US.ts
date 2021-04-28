948b53eddd80a448f49aaea19d516a931c617432
Simon Stürz <simon.stuerz@nymea.io>
2021-04-28 13:19:10 +0200

    Add translations for zigbee develco plugin

---
+++ zigbeedevelco/translations/1b9e976d-e842-40e1-9fad-87d71a69c721-en_US.ts
@@ -0,0 +1,202 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>ZigbeeDevelco</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="63"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="66"/>
+        <source>Connected</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: connected, ID: {16d5ebb7-21d1-4294-98a6-ca06eea8e3a3})
+----------
+The name of the StateType ({16d5ebb7-21d1-4294-98a6-ca06eea8e3a3}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="69"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the EventType ({16d5ebb7-21d1-4294-98a6-ca06eea8e3a3}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="72"/>
+        <source>Develco</source>
+        <extracomment>The name of the vendor ({e4b36f3e-ccdc-4a88-8968-39025c3ec742})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="75"/>
+        <source>IEEE adress</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, Type: thing, ID: {d9a3afa7-c460-43e7-bc84-c8b5ff1adf44})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="78"/>
+        <source>IO Module</source>
+        <extracomment>The name of the ThingClass ({3fb419ef-795d-4f4d-b801-e7eaff16cdb0})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="81"/>
+        <source>Identify</source>
+        <extracomment>The name of the ActionType ({62c5562a-8bdf-49b4-8e1d-f27442b2b360}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="84"/>
+        <source>Impulse duration</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, Type: settings, ID: {c2806b97-bf94-4ad2-ae22-5b5d7d1eaf5a})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="87"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="90"/>
+        <source>Input 1</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: input1, ID: {bc23c7e8-f4ad-47c4-b938-f9af2dbd3393})
+----------
+The name of the StateType ({bc23c7e8-f4ad-47c4-b938-f9af2dbd3393}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="93"/>
+        <source>Input 1 changed</source>
+        <extracomment>The name of the EventType ({bc23c7e8-f4ad-47c4-b938-f9af2dbd3393}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="96"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="99"/>
+        <source>Input 2</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: input2, ID: {065b0dc0-6d31-40ec-b356-02fea57e2fe1})
+----------
+The name of the StateType ({065b0dc0-6d31-40ec-b356-02fea57e2fe1}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="102"/>
+        <source>Input 2 changed</source>
+        <extracomment>The name of the EventType ({065b0dc0-6d31-40ec-b356-02fea57e2fe1}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="105"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="108"/>
+        <source>Input 3</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: input3, ID: {8f2b052a-30b7-49aa-a8d4-503cb0b9b66b})
+----------
+The name of the StateType ({8f2b052a-30b7-49aa-a8d4-503cb0b9b66b}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="111"/>
+        <source>Input 3 changed</source>
+        <extracomment>The name of the EventType ({8f2b052a-30b7-49aa-a8d4-503cb0b9b66b}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="114"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="117"/>
+        <source>Input 4</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: input4, ID: {caef986c-da13-4ef3-ab81-316244c7be1e})
+----------
+The name of the StateType ({caef986c-da13-4ef3-ab81-316244c7be1e}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="120"/>
+        <source>Input 4 changed</source>
+        <extracomment>The name of the EventType ({caef986c-da13-4ef3-ab81-316244c7be1e}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="123"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="126"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="129"/>
+        <source>Output 1</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, ActionType: output1, ID: {aaeda2c6-439a-452a-b829-45d6249bdee6})
+----------
+The name of the ParamType (ThingClass: ioModule, EventType: output1, ID: {aaeda2c6-439a-452a-b829-45d6249bdee6})
+----------
+The name of the StateType ({aaeda2c6-439a-452a-b829-45d6249bdee6}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="132"/>
+        <source>Output 1 changed</source>
+        <extracomment>The name of the EventType ({aaeda2c6-439a-452a-b829-45d6249bdee6}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="135"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="138"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="141"/>
+        <source>Output 2</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, ActionType: output2, ID: {105cf314-35b5-4a8b-8e6d-d011009f97ff})
+----------
+The name of the ParamType (ThingClass: ioModule, EventType: output2, ID: {105cf314-35b5-4a8b-8e6d-d011009f97ff})
+----------
+The name of the StateType ({105cf314-35b5-4a8b-8e6d-d011009f97ff}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="144"/>
+        <source>Output 2 changed</source>
+        <extracomment>The name of the EventType ({105cf314-35b5-4a8b-8e6d-d011009f97ff}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="147"/>
+        <source>Set output 1</source>
+        <extracomment>The name of the ActionType ({aaeda2c6-439a-452a-b829-45d6249bdee6}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="150"/>
+        <source>Set output 2</source>
+        <extracomment>The name of the ActionType ({105cf314-35b5-4a8b-8e6d-d011009f97ff}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="153"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="156"/>
+        <source>Signal strength</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: signalStrength, ID: {0e09e6a6-8b3f-4b63-acb1-ac04ab31957a})
+----------
+The name of the StateType ({0e09e6a6-8b3f-4b63-acb1-ac04ab31957a}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="159"/>
+        <source>Signal strength changed</source>
+        <extracomment>The name of the EventType ({0e09e6a6-8b3f-4b63-acb1-ac04ab31957a}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="162"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="165"/>
+        <source>Version</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, EventType: version, ID: {f28c9a61-b8ab-419c-bd85-58692df26ac7})
+----------
+The name of the StateType ({f28c9a61-b8ab-419c-bd85-58692df26ac7}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="168"/>
+        <source>Version changed</source>
+        <extracomment>The name of the EventType ({f28c9a61-b8ab-419c-bd85-58692df26ac7}) of ThingClass ioModule</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="171"/>
+        <source>Zigbee Develco</source>
+        <extracomment>The name of the plugin ZigbeeDevelco ({1b9e976d-e842-40e1-9fad-87d71a69c721})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/zigbeedevelco/plugininfo.h" line="174"/>
+        <source>Zigbee network UUID</source>
+        <extracomment>The name of the ParamType (ThingClass: ioModule, Type: thing, ID: {f4c83d2b-06e1-4206-98b7-e3ace79ff447})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
