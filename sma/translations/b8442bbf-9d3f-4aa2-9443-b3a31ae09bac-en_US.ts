78d9137311566f1d0daa6838b728d7d582703946
Simon Stürz <simon.stuerz@nymea.io>
2021-09-02 11:34:03 +0200

    Update translations

--- sma/translations/b8442bbf-9d3f-4aa2-9443-b3a31ae09bac-en_US.ts
+++ sma/translations/b8442bbf-9d3f-4aa2-9443-b3a31ae09bac-en_US.ts
@@ -2,127 +2,63 @@
 <!DOCTYPE TS>
 <TS version="2.1">
 <context>
+    <name>IntegrationPluginSma</name>
+    <message>
+        <location filename="../integrationpluginsma.cpp" line="45"/>
+        <source>Unable to discover devices in your network.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>sma</name>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="66"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="69"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="72"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="75"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="48"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="51"/>
         <source>Connected</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, EventType: connected, ID: {eda29c50-73ac-40e0-9c92-26fee352e688})
-----------
-The name of the StateType ({eda29c50-73ac-40e0-9c92-26fee352e688}) of ThingClass inverter
-----------
-The name of the ParamType (ThingClass: sunnyWebBox, EventType: connected, ID: {c05e6a1a-252c-4f2b-8b31-09cf113d01c1})
+        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: connected, ID: {c05e6a1a-252c-4f2b-8b31-09cf113d01c1})
 ----------
 The name of the StateType ({c05e6a1a-252c-4f2b-8b31-09cf113d01c1}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="78"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="81"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="54"/>
         <source>Connected changed</source>
-        <extracomment>The name of the EventType ({eda29c50-73ac-40e0-9c92-26fee352e688}) of ThingClass inverter
-----------
-The name of the EventType ({c05e6a1a-252c-4f2b-8b31-09cf113d01c1}) of ThingClass sunnyWebBox</extracomment>
+        <extracomment>The name of the EventType ({c05e6a1a-252c-4f2b-8b31-09cf113d01c1}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="84"/>
-        <source>Current Power changed</source>
-        <extracomment>The name of the EventType ({a804eabf-d5b8-4c83-84e4-8ec994875950}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="87"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="90"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="93"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="96"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="57"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="60"/>
         <source>Current power</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, EventType: currentPower, ID: {a804eabf-d5b8-4c83-84e4-8ec994875950})
-----------
-The name of the StateType ({a804eabf-d5b8-4c83-84e4-8ec994875950}) of ThingClass inverter
-----------
-The name of the ParamType (ThingClass: sunnyWebBox, EventType: currentPower, ID: {ff4ff872-2f0f-4ca4-9fe2-220eeaf16cc2})
+        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: currentPower, ID: {ff4ff872-2f0f-4ca4-9fe2-220eeaf16cc2})
 ----------
 The name of the StateType ({ff4ff872-2f0f-4ca4-9fe2-220eeaf16cc2}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="99"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="63"/>
         <source>Current power changed</source>
         <extracomment>The name of the EventType ({ff4ff872-2f0f-4ca4-9fe2-220eeaf16cc2}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="102"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="105"/>
-        <source>Day energy</source>
-        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: dayEnergy, ID: {16f34c5c-8dbb-4dcc-9faa-4b782d57226c})
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="66"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="69"/>
+        <source>Day energy produced</source>
+        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: dayEnergyProduced, ID: {16f34c5c-8dbb-4dcc-9faa-4b782d57226c})
 ----------
 The name of the StateType ({16f34c5c-8dbb-4dcc-9faa-4b782d57226c}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="108"/>
-        <source>Day energy changed</source>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="72"/>
+        <source>Day energy produced changed</source>
         <extracomment>The name of the EventType ({16f34c5c-8dbb-4dcc-9faa-4b782d57226c}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="111"/>
-        <source>Device ID</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, Type: thing, ID: {f43e8159-7337-4bd0-ba74-b6630e554e43})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="114"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="117"/>
-        <source>Energy of current day</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, EventType: eday, ID: {cd3a0abc-37cc-4f9c-8ff2-c7ccc2513880})
-----------
-The name of the StateType ({cd3a0abc-37cc-4f9c-8ff2-c7ccc2513880}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="120"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="123"/>
-        <source>Energy of current year</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, EventType: eyear, ID: {341596ec-690b-4bf6-b6ed-0f92fa1c7d6c})
-----------
-The name of the StateType ({341596ec-690b-4bf6-b6ed-0f92fa1c7d6c}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="126"/>
-        <source>Energy of day changed</source>
-        <extracomment>The name of the EventType ({cd3a0abc-37cc-4f9c-8ff2-c7ccc2513880}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="129"/>
-        <source>Energy of year changed</source>
-        <extracomment>The name of the EventType ({341596ec-690b-4bf6-b6ed-0f92fa1c7d6c}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="132"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="135"/>
-        <source>Energy total</source>
-        <extracomment>The name of the ParamType (ThingClass: inverter, EventType: totalEnergyProduced, ID: {6b6c1ddb-692f-400f-8b8d-38e0e0ae34ba})
-----------
-The name of the StateType ({6b6c1ddb-692f-400f-8b8d-38e0e0ae34ba}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="138"/>
-        <source>Energy total changed</source>
-        <extracomment>The name of the EventType ({6b6c1ddb-692f-400f-8b8d-38e0e0ae34ba}) of ThingClass inverter</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="141"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="144"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="75"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="78"/>
         <source>Error</source>
         <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: error, ID: {4e64f9ca-7e5a-4897-8035-6f2ae88fde89})
 ----------
@@ -130,20 +66,20 @@ The name of the StateType ({4e64f9ca-7e5a-4897-8035-6f2ae88fde89}) of ThingClass
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="147"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="81"/>
         <source>Error changed</source>
         <extracomment>The name of the EventType ({4e64f9ca-7e5a-4897-8035-6f2ae88fde89}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="150"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="84"/>
         <source>Host address</source>
         <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, Type: thing, ID: {864d4162-e3ce-48b8-b8ac-c1b971b52d42})</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="153"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="156"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="87"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="90"/>
         <source>Mode</source>
         <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: mode, ID: {1974550b-6059-4b0e-83f4-70177e20dac3})
 ----------
@@ -151,14 +87,14 @@ The name of the StateType ({1974550b-6059-4b0e-83f4-70177e20dac3}) of ThingClass
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="159"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="93"/>
         <source>Mode changed</source>
         <extracomment>The name of the EventType ({1974550b-6059-4b0e-83f4-70177e20dac3}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="162"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="165"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="96"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="99"/>
         <source>SMA</source>
         <extracomment>The name of the vendor ({16d5a4a3-36d5-46c0-b7dd-df166ddf5981})
 ----------
@@ -166,40 +102,28 @@ The name of the plugin sma ({b8442bbf-9d3f-4aa2-9443-b3a31ae09bac})</extracommen
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="168"/>
-        <source>Search new devices</source>
-        <extracomment>The name of the ActionType ({15fc3dac-1868-4490-ba41-7c2f545926ad}) of ThingClass sunnyWebBox</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="171"/>
-        <source>Solar Inverter</source>
-        <extracomment>The name of the ThingClass ({9cb72321-042e-4912-b23e-18516b6bbe96})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="174"/>
-        <source>Sunny WebBox</source>
-        <extracomment>The name of the ThingClass ({49304127-ce9b-45dd-8511-05030a4ac003})</extracomment>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="177"/>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="180"/>
-        <source>Total energy</source>
-        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: totalEnergy, ID: {0bb4e227-7e38-49ca-9b32-ce4621c9305b})
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="105"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="108"/>
+        <source>Total energy produced</source>
+        <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, EventType: totalEnergyProduced, ID: {0bb4e227-7e38-49ca-9b32-ce4621c9305b})
 ----------
 The name of the StateType ({0bb4e227-7e38-49ca-9b32-ce4621c9305b}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="183"/>
-        <source>Total energy changed</source>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="111"/>
+        <source>Total energy produced changed</source>
         <extracomment>The name of the EventType ({0bb4e227-7e38-49ca-9b32-ce4621c9305b}) of ThingClass sunnyWebBox</extracomment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="186"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="102"/>
+        <source>Sunny WebBox</source>
+        <extracomment>The name of the ThingClass ({49304127-ce9b-45dd-8511-05030a4ac003})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sma/plugininfo.h" line="114"/>
         <source>hardware address</source>
         <extracomment>The name of the ParamType (ThingClass: sunnyWebBox, Type: thing, ID: {03f32361-4e13-4597-a346-af8d16a986b3})</extracomment>
         <translation type="unfinished"></translation>
