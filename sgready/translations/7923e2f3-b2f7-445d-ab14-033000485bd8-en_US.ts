b4daf5a693e49cc78cb9d06a70bc98aeb9fb68a0
Simon Stürz <simon.stuerz@nymea.io>
2021-10-12 14:56:52 +0200

    Add translations

---
+++ sgready/translations/7923e2f3-b2f7-445d-ab14-033000485bd8-en_US.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>IntegrationPluginSgReady</name>
+    <message>
+        <location filename="../integrationpluginsgready.cpp" line="52"/>
+        <source>No GPIOs available on this system.</source>
+        <extracomment>Error discovering SG ready GPIOs</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginsgready.cpp" line="69"/>
+        <source>No GPIOs found on this system.</source>
+        <extracomment>Error discovering SG ready GPIOs</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../integrationpluginsgready.cpp" line="89"/>
+        <source>Failed to set up the GPIO hardware interface.</source>
+        <extracomment>Error message if SG ready GPIOs setup failed</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SgReady</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="41"/>
+        <source>GPIO number 1</source>
+        <extracomment>The name of the ParamType (ThingClass: sgReadyInterface, Type: thing, ID: {8de25aef-d868-4410-abd3-3bec9c46aba3})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="44"/>
+        <source>GPIO number 2</source>
+        <extracomment>The name of the ParamType (ThingClass: sgReadyInterface, Type: thing, ID: {113ba124-a392-4bc3-861a-f7feaab82fc4})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="47"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="50"/>
+        <source>Relay 1 enabled</source>
+        <extracomment>The name of the ParamType (ThingClass: sgReadyInterface, EventType: gpio1State, ID: {d2c83778-693f-4146-a656-a8ed901c1a7f})
+----------
+The name of the StateType ({d2c83778-693f-4146-a656-a8ed901c1a7f}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="53"/>
+        <source>Relay 1 enabled changed</source>
+        <extracomment>The name of the EventType ({d2c83778-693f-4146-a656-a8ed901c1a7f}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="56"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="59"/>
+        <source>Relay 2 enabled</source>
+        <extracomment>The name of the ParamType (ThingClass: sgReadyInterface, EventType: gpio2State, ID: {15608d58-b1d4-4696-ac1a-62aec5ec44ab})
+----------
+The name of the StateType ({15608d58-b1d4-4696-ac1a-62aec5ec44ab}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="62"/>
+        <source>Relay 2 enabled changed</source>
+        <extracomment>The name of the EventType ({15608d58-b1d4-4696-ac1a-62aec5ec44ab}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="65"/>
+        <source>SG Ready interface</source>
+        <extracomment>The name of the ThingClass ({5a5f3aca-a958-4bca-b8fc-1168fe3371ec})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="68"/>
+        <source>SG-Ready</source>
+        <extracomment>The name of the plugin SgReady ({7923e2f3-b2f7-445d-ab14-033000485bd8})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="71"/>
+        <source>Set smart grid mode</source>
+        <extracomment>The name of the ActionType ({91008e09-2702-4bd5-942e-398513ae3d15}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="74"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="77"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="80"/>
+        <source>Smart grid mode</source>
+        <extracomment>The name of the ParamType (ThingClass: sgReadyInterface, ActionType: sgReadyMode, ID: {91008e09-2702-4bd5-942e-398513ae3d15})
+----------
+The name of the ParamType (ThingClass: sgReadyInterface, EventType: sgReadyMode, ID: {91008e09-2702-4bd5-942e-398513ae3d15})
+----------
+The name of the StateType ({91008e09-2702-4bd5-942e-398513ae3d15}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="83"/>
+        <source>Smart grid mode changed</source>
+        <extracomment>The name of the EventType ({91008e09-2702-4bd5-942e-398513ae3d15}) of ThingClass sgReadyInterface</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/sgready/plugininfo.h" line="86"/>
+        <source>nymea</source>
+        <extracomment>The name of the vendor ({2062d64d-3232-433c-88bc-0d33c0ba2ba6})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
