a4259205de8603ed73a65e14444b08105b72b3da
Bruce Eckstein <aero_nextgen@verizon.net>
2021-10-31 07:14:20 -0400

    Garadget update to include all files

---
+++ garadget/README.md
@@ -0,0 +1,51 @@
+The nymea plugin for Garadget Garage Door Opener requires Garadget version 1.20 or later 
+nymea supports the Garadget device via the mqtt only mode using the nymea internal MQTT broker. External MQTT broker is not supported.
+    Make sure nymeas MQTT broker is enabled in System Settings -> MQTT broker and create a policy (Client ID, username and password) for the garadget device to log in.
+    
+Note: The garadget must be configured with the MQTT mode only! otherwise the Particle operation will probably conflict.
+
+See the (https://www.garadget.com/setup-instructions/) for garadget general setup and (https://community.garadget.com/t/mqtt-support/3226/5) for MQTT instructions
+
+Garadget Initial Configuration:
+    Put your device into listening mode: press and hold “M” button for about 3 seconds until LED starts blinking dark blue.
+    Connect any WiFi enabled device to PHOTON-XXXX access point.
+    Open http://192.168.0.1/ in the browser 
+        Confirmed in settings that your Garadget is v1.20 or later.
+    at a minimum set the following:
+        select your SSID
+        enter SSID password
+        select Only MQTT
+        enter nymead Broker IP: 
+        enter nymead Broker Port:
+        enter nymead Broker User ID: (if needed)
+        enter nymead Broker Password: (if needed)
+        enter Device Topic ID: (this must be unique across all garadget devices and contain NO /s allowed)
+    select Save & Connect (The device will reconnect to your WiFi network via dhcp)
+
+nymea configuration:
+    assuming you have installed the garadget plugin:
+    select garadget plugin from the ADD things page or Configure things page. 
+    select Garadget (there will be an Internal MQTT client and MQTT client - Ignore them)
+    Name the thing with your choice of names
+    Thing Parameter -> enter the Device Topic ID: you entered in the Garadget device above.
+
+nymea operation:
+    on the Garadget window, the up icon, stop icon, and down icon cause the device to activate the garage door.
+    on the Garadget detail window (uppre righthand corner, you can tune the Garadget configuration for:
+        Refection Threshold
+        Button Press Time
+        Door Moving Time. (you should adjust to the time it takes your door to open)
+    when changed it takes a couple seconds for Garadget to respond and update the values shown in the nymea window.
+
+notes:
+    Publish does not send any messages.
+    other than the three types shown above and the open, close and stop items, no action is taken by the plugin.
+    DECKO Garage Door opener requires a ~ 3.3 Volt zener diode in series of the connection between the Garadget and the DECKO
+
+Issues: Garadget operating a DECKO garage door opener (may not be issue with other garage door openers)
+    the Garadget can get confused on the correct relay actions to take if you hit "stop" and then either "open" or "close" when operating a DECKO 
+    It is best to be in direct view of the door if you hit "stop" (the plugin image will be halfway up) to be sure you know where the door will go next.
+    This confusion is an issue of the Garadget and not of nymea-plugin.
+
+    The plugin will show connected as soon as the Garadget connects to the broker.
+    The plugin does NOT know if the Garadget disconnects and therefore will continue to show connected even if the Garadget is no longer connected.
---
+++ garadget/translations/476781af-ad41-423b-b9bb-f308f2aff339-en_US.ts
@@ -0,0 +1,235 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>garadget</name>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="70"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="73"/>
+        <source>Ambient Brightness Level</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: brightlevel, ID: {bd7fece8-3ff6-421a-bb5e-80323beb8d8d})
+----------
+The name of the StateType ({bd7fece8-3ff6-421a-bb5e-80323beb8d8d}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="76"/>
+        <source>Ambient Brightness changed</source>
+        <extracomment>The name of the EventType ({bd7fece8-3ff6-421a-bb5e-80323beb8d8d}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="79"/>
+        <source>Button Press Time changed</source>
+        <extracomment>The name of the EventType ({c38e89db-b259-4fa0-9f20-03fe3b8a7514}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="82"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="85"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="88"/>
+        <source>Button Press Time in 10-2000 ms</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: rlt, ID: {c38e89db-b259-4fa0-9f20-03fe3b8a7514})
+----------
+The name of the ParamType (ThingClass: garadget, EventType: rlt, ID: {c38e89db-b259-4fa0-9f20-03fe3b8a7514})
+----------
+The name of the StateType ({c38e89db-b259-4fa0-9f20-03fe3b8a7514}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="91"/>
+        <source>Close</source>
+        <extracomment>The name of the ActionType ({56a9e131-b65e-4622-9a9b-f5b42d0021ef}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="94"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="97"/>
+        <source>Connected</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: connected, ID: {a3d7e6eb-82d0-47ee-b95f-4dde931eb7e2})
+----------
+The name of the StateType ({a3d7e6eb-82d0-47ee-b95f-4dde931eb7e2}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="100"/>
+        <source>Connected changed</source>
+        <extracomment>The name of the EventType ({a3d7e6eb-82d0-47ee-b95f-4dde931eb7e2}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="103"/>
+        <source>Door Moving Time changed</source>
+        <extracomment>The name of the EventType ({acda9268-4663-46d1-a409-231d648e6fc8}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="106"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="109"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="112"/>
+        <source>Door Moving Time in 1-120 Sec</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: mtt, ID: {acda9268-4663-46d1-a409-231d648e6fc8})
+----------
+The name of the ParamType (ThingClass: garadget, EventType: mtt, ID: {acda9268-4663-46d1-a409-231d648e6fc8})
+----------
+The name of the StateType ({acda9268-4663-46d1-a409-231d648e6fc8}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="115"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="118"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="121"/>
+        <source>Garadget</source>
+        <extracomment>The name of the ThingClass ({e808b8ae-7608-41ce-8444-892f0648a4d3})
+----------
+The name of the vendor ({22d59bee-79cc-4dea-b742-30ac6bdcf2c9})
+----------
+The name of the plugin garadget ({476781af-ad41-423b-b9bb-f308f2aff339})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="124"/>
+        <source>Open</source>
+        <extracomment>The name of the ActionType ({ae3b1c67-7219-4768-a1c7-55c3b9a610cb}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="127"/>
+        <source>Payload</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: trigger, ID: {088e7db5-c814-4165-b9ff-a411a4c20f19})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="130"/>
+        <source>Playload</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: triggered, ID: {7573d3c5-a867-4123-a7f9-65eb35eac233})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="133"/>
+        <source>Publish</source>
+        <extracomment>The name of the ActionType ({4cde52f7-78f9-49a4-b4c3-76bd702bc473}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="136"/>
+        <source>Publish received</source>
+        <extracomment>The name of the EventType ({a42983a9-9194-458d-9e67-fc4d7ce27e40}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="139"/>
+        <source>QoS</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: trigger, ID: {140d859b-87b7-44f0-b54f-a11846e24985})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="142"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="145"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="148"/>
+        <source>Reflection Threshold 0-80</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: srt, ID: {656a181e-5c06-4bb6-8c5d-7921db07bef6})
+----------
+The name of the ParamType (ThingClass: garadget, EventType: srt, ID: {656a181e-5c06-4bb6-8c5d-7921db07bef6})
+----------
+The name of the StateType ({656a181e-5c06-4bb6-8c5d-7921db07bef6}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="151"/>
+        <source>Reflection Threshold changed</source>
+        <extracomment>The name of the EventType ({656a181e-5c06-4bb6-8c5d-7921db07bef6}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="154"/>
+        <source>Retain message</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: trigger, ID: {44079c1a-9a0a-426d-9508-5b2dcbe40a90})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="157"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="160"/>
+        <source>Sensor Level</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: sensorlevel, ID: {4aaeefbd-46d9-4111-b262-a001a47ecb22})
+----------
+The name of the StateType ({4aaeefbd-46d9-4111-b262-a001a47ecb22}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="163"/>
+        <source>Sensor Level changed</source>
+        <extracomment>The name of the EventType ({4aaeefbd-46d9-4111-b262-a001a47ecb22}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="166"/>
+        <source>Set Button Press Time value</source>
+        <extracomment>The name of the ActionType ({c38e89db-b259-4fa0-9f20-03fe3b8a7514}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="169"/>
+        <source>Set Door Moving Time value</source>
+        <extracomment>The name of the ActionType ({acda9268-4663-46d1-a409-231d648e6fc8}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="172"/>
+        <source>Set Reflection Threshold value</source>
+        <extracomment>The name of the ActionType ({656a181e-5c06-4bb6-8c5d-7921db07bef6}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="175"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="178"/>
+        <source>Signal Level</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: signallevel, ID: {024f178c-a920-42d4-887f-1c90a96d5eb3})
+----------
+The name of the StateType ({024f178c-a920-42d4-887f-1c90a96d5eb3}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="181"/>
+        <source>Signal Level changed</source>
+        <extracomment>The name of the EventType ({024f178c-a920-42d4-887f-1c90a96d5eb3}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="184"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="187"/>
+        <source>State</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, EventType: state, ID: {174509b5-fc3e-4f03-8c93-8e044f0a2ce0})
+----------
+The name of the StateType ({174509b5-fc3e-4f03-8c93-8e044f0a2ce0}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="190"/>
+        <source>State changed</source>
+        <extracomment>The name of the EventType ({174509b5-fc3e-4f03-8c93-8e044f0a2ce0}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="193"/>
+        <source>Stop</source>
+        <extracomment>The name of the ActionType ({12ea84c1-0650-4ac1-a02c-de95965def92}) of ThingClass garadget</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="196"/>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="199"/>
+        <source>Topic</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, ActionType: trigger, ID: {9208d419-82d4-4806-a15d-210991daf0ac})
+----------
+The name of the ParamType (ThingClass: garadget, EventType: triggered, ID: {5dfb6ce9-095b-4476-ac94-c7c35653de1f})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../../build-nymea-plugins-Desktop-Debug/garadget/plugininfo.h" line="202"/>
+        <source>deviceName</source>
+        <extracomment>The name of the ParamType (ThingClass: garadget, Type: thing, ID: {54a11a17-fc37-4316-891f-001c55e38220})</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
