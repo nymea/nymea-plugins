b88935bd2fbae6a235b95fbf3d549f913301d551
Martin Lukas <martin.lukas@chargebyte.com>
2024-10-28 09:22:06 +0100

    Rework after review
    Signed-off-by: Martin Lukas <martin.lukas@chargebyte.com>

--- shelly/integrationpluginshelly.h
+++ shelly/integrationpluginshelly.h
@@ -94,7 +94,7 @@ private:
     PluginTimer *m_reconfigureTimer = nullptr;
 
     Coap *m_coap = nullptr;
-    uint m_multicastRetryCnt = 0;
+    uint m_multicastWarningPrintCount = 0;
 
     QHash<Thing*, ShellyJsonRpcClient*> m_rpcClients;
 };
