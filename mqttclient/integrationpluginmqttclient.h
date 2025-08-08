c323e0ffd7e9123bd824fb76744839004b22cd7c
Simon Stürz <simon.stuerz@nymea.io>
2025-08-08 14:49:01 +0200

    mqttclient: Add Qt6 support

--- mqttclient/integrationpluginmqttclient.h
+++ mqttclient/integrationpluginmqttclient.h
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2020, nymea GmbH
+* Copyright 2013 - 2025, nymea GmbH
 * Contact: contact@nymea.io
 *
 * This file is part of nymea.
@@ -31,7 +31,7 @@
 #ifndef INTEGRATIONPLUGINMQTTCLIENT_H
 #define INTEGRATIONPLUGINMQTTCLIENT_H
 
-#include "integrations/integrationplugin.h"
+#include <integrations/integrationplugin.h>
 
 #include <QHash>
 #include <QDebug>
