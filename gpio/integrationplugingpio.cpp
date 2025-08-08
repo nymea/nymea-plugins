d2eacc95a118cc923fcc40167fe032645af68ca3
Simon Stürz <simon.stuerz@nymea.io>
2025-08-08 13:39:17 +0200

    gpio: Add Qt6 support

--- gpio/integrationplugingpio.cpp
+++ gpio/integrationplugingpio.cpp
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2021, nymea GmbH
+* Copyright 2013 - 2025, nymea GmbH
 * Contact: contact@nymea.io
 *
 * This file is part of nymea.
@@ -29,9 +29,10 @@
 * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include "integrationplugingpio.h"
-#include "types/param.h"
-#include "integrations/thing.h"
 #include "plugininfo.h"
+
+#include <types/param.h>
+#include <integrations/thing.h>
 
 IntegrationPluginGpio::IntegrationPluginGpio()
 {
