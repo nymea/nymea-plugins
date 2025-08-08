b16a71401a20720897bfa4b1b47486db6757c41b
Simon Stürz <simon.stuerz@nymea.io>
2025-08-08 15:52:36 +0200

    powerfox: Add Qt6 support

--- powerfox/integrationpluginpowerfox.h
+++ powerfox/integrationpluginpowerfox.h
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2022, nymea GmbH
+* Copyright 2013 - 2025, nymea GmbH
 * Contact: contact@nymea.io
 *
 * This file is part of nymea.
@@ -31,7 +31,8 @@
 #ifndef INTEGRATIONPLUGINPOWERFOX_H
 #define INTEGRATIONPLUGINPOWERFOX_H
 
-#include "integrations/integrationplugin.h"
+#include <integrations/integrationplugin.h>
+
 #include "extern-plugininfo.h"
 
 #include <QUrlQuery>
