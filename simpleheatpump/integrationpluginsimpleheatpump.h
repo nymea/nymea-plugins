1ffb4626772e0d52914c7b63d392123ecc497363
Simon Stürz <simon.stuerz@nymea.io>
2025-08-08 16:19:19 +0200

    simpleheatpump: Add Qt6 support

--- simpleheatpump/integrationpluginsimpleheatpump.h
+++ simpleheatpump/integrationpluginsimpleheatpump.h
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 *
-* Copyright 2013 - 2021, nymea GmbH
+* Copyright 2013 - 2025, nymea GmbH
 * Contact: contact@nymea.io
 *
 * This file is part of nymea.
@@ -31,8 +31,7 @@
 #ifndef INTEGRATIONPLUGINSIMPLEHEATPUMP_H
 #define INTEGRATIONPLUGINSIMPLEHEATPUMP_H
 
-#include "integrations/integrationplugin.h"
-
+#include <integrations/integrationplugin.h>
 #include <gpio.h>
 
 class IntegrationPluginSimpleHeatpump : public IntegrationPlugin
